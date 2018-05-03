04ade8b973efba35ba187553aa6390b3672dcda8
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-02 19:08:04 -0600

    Added coverage test files.

---
+++ Resources/SDGXcodeTests/Source.swift
@@ -0,0 +1,30 @@
+/*
+ Source.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+func covered() -> Bool {
+    return true
+}
+
+func notCovered() -> Bool {
+    return true
+}
+
+var `switch` = false
+func branching() -> Bool {
+    if `switch` {
+        return true
+    } else {
+        return true
+    }
+}
