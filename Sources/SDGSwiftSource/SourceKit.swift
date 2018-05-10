89f57628da62a89ecf99eeb6eb73bb83f28c050b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-09 23:20:08 -0600

    Refactored response.

--- Sources/SDGSwiftSource/SourceKit.swift
+++ Sources/SDGSwiftSource/SourceKit.swift
@@ -57,22 +57,7 @@ public enum SourceKit {
 
     // MARK: - Symbols
 
-    // Object
-
-    private static func sourcekitd_request_array_create(_ objects: UnsafePointer<sourcekitd_object_t?>?, count: Int) throws -> sourcekitd_object_t? {
-        return (try load(symbol: "sourcekitd_request_array_create") as (@convention(c) (UnsafePointer<sourcekitd_object_t?>?, Int) -> sourcekitd_object_t?))(objects, count)
-    }
-
     // Response
-
-    private typealias sourcekitd_response_t = UnsafeMutableRawPointer
-    private static func sourcekitd_response_dispose(_ object: sourcekitd_response_t) throws {
-        (try load(symbol: "sourcekitd_response_dispose") as (@convention(c) (sourcekitd_response_t) -> Void))(object)
-    }
-
-    private static func sourcekitd_send_request_sync(_ request: sourcekitd_object_t) throws -> sourcekitd_response_t? {
-        return (try load(symbol: "sourcekitd_send_request_sync") as (@convention(c) (sourcekitd_object_t) -> sourcekitd_response_t?))(request)
-    }
 
     private static func sourcekitd_response_is_error(_ object: sourcekitd_response_t) throws -> Bool {
         return (try load(symbol: "sourcekitd_response_is_error") as (@convention(c) (sourcekitd_response_t) -> Bool))(object)
@@ -86,14 +71,12 @@ public enum SourceKit {
 
     public static func test() throws {
         // [_Warning: Temporary._]
-        let literal: [UID: Object] = [
+        let response = try Response(toRequest: try Object([
             try UID("key.request"): try Object(UID("source.request.indexsource")),
             try UID("key.sourcetext"): try Object("print(\"Hello, world!\")")
-        ]
-        let request = try Object(literal)
-        let result = try sourcekitd_send_request_sync(request.rawValue)!
-        if try sourcekitd_response_is_error(result) {
-            let description = try sourcekitd_response_error_get_description(result)!
+            ]))
+        if try sourcekitd_response_is_error(response.rawValue) {
+            let description = try sourcekitd_response_error_get_description(response.rawValue)!
             print(String(validatingUTF8: description)!)
         } else {
             print("No error.")
--- Sources/SDGSwiftSource/SourceKitObject.swift
+++ Sources/SDGSwiftSource/SourceKitObject.swift
@@ -26,21 +26,13 @@ extension SourceKit {
         }
 
         deinit {
-            try? Object.release(rawValue)
+            if let release = (try? load(symbol: "sourcekitd_request_release") as (@convention(c) (sourcekitd_object_t) -> Void)) {
+                release(rawValue)
+            }
         }
 
         // MARK: - Properties
 
         internal let rawValue: sourcekitd_object_t
-
-        // MARK: - Memory Management
-
-        private static func retain(_ rawValue: sourcekitd_object_t) throws -> sourcekitd_object_t {
-            return (try load(symbol: "sourcekitd_request_retain") as (@convention(c) (sourcekitd_object_t) -> (sourcekitd_object_t?)))(rawValue)!
-        }
-
-        private static func release(_ rawValue: sourcekitd_object_t) throws {
-            (try load(symbol: "sourcekitd_request_release") as (@convention(c) (sourcekitd_object_t) -> Void))(rawValue)
-        }
     }
 }
