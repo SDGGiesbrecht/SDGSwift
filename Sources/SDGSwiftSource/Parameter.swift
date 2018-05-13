d092dd55ff0dd489a1eba8cad082fa3f728d73c1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-12 23:01:44 -0600

    Refreshed.

--- Sources/SDGSwiftSource/Parameter.swift
+++ Sources/SDGSwiftSource/Parameter.swift
@@ -1,13 +1,26 @@
+/*
+ Parameter.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 /// A parameter.
 public class Parameter : ContainerSyntaxElement {
-    
+
     internal init(substructureInformation: SourceKit.Variant, in source: String) throws {
         name = Identifier(range: try SyntaxElement.range(from: substructureInformation, for: "key.name", in: source))
         try super.init(substructureInformation: substructureInformation, in: source, knownChildren: [name])
     }
-    
+
     // MARK: - Properties
-    
+
     let name: Identifier
 }
--- Sources/SDGSwiftSource/VariableDeclaration.swift
+++ Sources/SDGSwiftSource/VariableDeclaration.swift
@@ -1,14 +1,26 @@
+/*
+ VariableDeclaration.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 /// A variable declaration.
 public class VariableDeclaration : ContainerSyntaxElement {
-    
+
     internal init(substructureInformation: SourceKit.Variant, in source: String) throws {
         name = Identifier(range: try SyntaxElement.range(from: substructureInformation, for: "key.name", in: source))
         try super.init(substructureInformation: substructureInformation, in: source, knownChildren: [name])
-        print(substructureInformation.asAny())
     }
-    
+
     // MARK: - Properties
-    
+
     let name: Identifier
 }
