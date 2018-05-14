aaa0ff9eb1f869c5d1dae07097220ac40b132372
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-13 22:05:58 -0600

    Detected “var”.

--- Sources/SDGSwiftSource/Syntax Elements/Container Elements/VariableDeclaration.swift
+++ Sources/SDGSwiftSource/Syntax Elements/Container Elements/VariableDeclaration.swift
@@ -12,15 +12,31 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
+
 /// A variable declaration.
 public class VariableDeclaration : ContainerSyntaxElement {
 
     internal init(substructureInformation: SourceKit.Variant, source: String, tokens: [SourceKit.PrimitiveToken]) throws {
         name = Identifier(range: try SyntaxElement.range(from: substructureInformation, for: "key.name", in: source), isDefinition: true)
         try super.init(substructureInformation: substructureInformation, source: source, tokens: tokens, knownChildren: [name])
+
+        for child in children where child is UnidentifiedSyntaxElement {
+            if let match = source.scalars.firstMatch(for: "var".scalars, in: child.range) {
+
+                let type = Keyword(range: match.range)
+                let structure = children.filter({ ¬($0 is UnidentifiedSyntaxElement) })
+                children = structure + [type]
+                mutability = type
+                break
+            }
+        }
     }
 
     // MARK: - Properties
 
-    let name: Identifier
+    /// The variable’s mutability. (i.e. `let` or `var`)
+    public private(set) var mutability: Keyword?
+    /// The name of the variable.
+    public let name: Identifier
 }
