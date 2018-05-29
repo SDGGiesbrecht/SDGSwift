4c836251a11404b2931a4e4d3a6f8162cc93f8de
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-05-29 17:10:53 -0600

    Fixed unlabelled parameters.

--- Sources/SDGSwiftSource/Syntax Elements/Container Elements/Parameter.swift
+++ Sources/SDGSwiftSource/Syntax Elements/Container Elements/Parameter.swift
@@ -18,12 +18,18 @@ import SDGLogic
 public class Parameter : ContainerSyntaxElement {
 
     internal init(substructureInformation: SourceKit.Variant, source: String, tokens: [SourceKit.PrimitiveToken]) throws {
-        name = Identifier(range: try SyntaxElement.range(from: substructureInformation, for: "key.name", in: source), isDefinition: true)
-        try super.init(substructureInformation: substructureInformation, source: source, tokens: tokens, knownChildren: [name])
+        let possibleLabel = Identifier(range: try SyntaxElement.range(from: substructureInformation, for: "key.name", in: source), isDefinition: true)
+        if ¬possibleLabel.range.isEmpty {
+            label = possibleLabel
+            try super.init(substructureInformation: substructureInformation, source: source, tokens: tokens, knownChildren: [possibleLabel])
+        } else {
+            label = nil
+            try super.init(substructureInformation: substructureInformation, source: source, tokens: tokens, knownChildren: [])
+        }
     }
 
     // MARK: - Properties
 
-    /// The name of the parameter.
-    public let name: Identifier
+    /// The label of the parameter.
+    public let label: Identifier?
 }
