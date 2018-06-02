a7a61f84f20a65c3b8b2b8d1f46a63200a98dbe3
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-01 23:00:06 -0600

    Refreshed.

--- Sources/SDGSwiftSource/Syntax Elements/Container Elements/EnumerationCase.swift
+++ Sources/SDGSwiftSource/Syntax Elements/Container Elements/EnumerationCase.swift
@@ -1,3 +1,16 @@
+/*
+ EnumerationCase.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
 
 /// An enumeration case declaration.
 public class EnumerationCase : ContainerSyntaxElement {
--- Sources/SDGSwiftSource/Syntax Elements/Container Elements/SwitchCase.swift
+++ Sources/SDGSwiftSource/Syntax Elements/Container Elements/SwitchCase.swift
@@ -1,5 +1,5 @@
 /*
- Case.swift
+ SwitchCase.swift
 
  This source file is part of the SDGSwift open source project.
  https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
