5ea038800b569ef59979b4ab7930897dc4f2271f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-03 20:30:51 -0600

    Added property.

--- Sources/SampleConfiguration/SampleConfiguration.swift
+++ Sources/SampleConfiguration/SampleConfiguration.swift
@@ -11,3 +11,20 @@
  Licensed under the Apache Licence, Version 2.0.
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
+
+import SDGSwiftConfiguration
+
+/// A sample configuration.
+public final class SampleConfiguration : Configuration {
+
+    // MARK: - Properties
+
+    /// A customizable option.
+    public var option: String
+
+    // MARK: - Configuration
+
+    public init() {
+        option = "Default"
+    }
+}
