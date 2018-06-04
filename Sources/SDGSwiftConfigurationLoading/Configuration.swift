8af9869e969db32c9e8a3831dce052e8d8e0ce0e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-03 20:12:25 -0600

    Regenerated test list.

--- Sources/SDGSwiftConfigurationLoading/Configuration.swift
+++ Sources/SDGSwiftConfigurationLoading/Configuration.swift
@@ -1,5 +1,5 @@
 /*
- Exports.swift
+ Configuration.swift
 
  This source file is part of the SDGSwift open source project.
  https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
--- Tests/SDGSwiftConfigurationTests/SDGSwiftConfigurationAPITests.swift
+++ Tests/SDGSwiftConfigurationTests/SDGSwiftConfigurationAPITests.swift
@@ -1,5 +1,5 @@
 /*
- SDGSwiftSourceAPITests.swift
+ SDGSwiftConfigurationAPITests.swift
 
  This source file is part of the SDGSwift open source project.
  https://sdggiesbrecht.github.io/SDGSwift/SDGSwift
