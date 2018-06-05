8ebbd188326f4da192d8081877557f09a8e9d102
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-04 22:46:17 -0600

    Enabled configuration loading.

--- Package.swift
+++ Package.swift
@@ -39,8 +39,7 @@ let package = Package(
         /// Utilities for defining configuration files written in Swift (similar to package manifests).
         .library(name: "SDGSwiftConfiguration", targets: ["SDGSwiftConfiguration"]),
         /// Utilities for loading configuration files written in Swift (similar to package manifests).
-        // [_Workaround: Needs a stable release of SDGSwiftConfiguration to depend on._]
-        //.library(name: "SDGSwiftConfigurationLoading", targets: ["SDGSwiftConfigurationLoading"])
+        .library(name: "SDGSwiftConfigurationLoading", targets: ["SDGSwiftConfigurationLoading"]),
         /// A sample configuration demonstrating the use of `SDGSwiftConfiguration` and `SDGSwiftConfigurationLoading`.
         .library(name: "SampleConfiguration", targets: ["SampleConfiguration"])
     ],
