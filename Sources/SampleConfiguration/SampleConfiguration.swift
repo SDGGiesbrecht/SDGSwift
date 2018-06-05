cda8339ecdcce2b621171a43ce89a1292c6900ce
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-04 19:42:05 -0600

    Documented sample.

--- Sources/SampleConfiguration/SampleConfiguration.swift
+++ Sources/SampleConfiguration/SampleConfiguration.swift
@@ -28,6 +28,7 @@ public final class SampleConfiguration : Configuration, Equatable {
         case option
     }
 
+    /// Encodes the configuration.
     public override func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(option, forKey: .option)
@@ -35,6 +36,7 @@ public final class SampleConfiguration : Configuration, Equatable {
         try super.encode(to: container.superEncoder())
     }
 
+    /// Decodes a configuration.
     public required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         try super.init(from: container.superDecoder())
@@ -46,12 +48,14 @@ public final class SampleConfiguration : Configuration, Equatable {
 
     // MARK: - Configuration
 
+    /// Creates the default configuration.
     public required init() {
         super.init()
     }
 
     // MARK: - Equatable
 
+    /// Compares the configurations for equality.
     public static func == (precedingValue: SampleConfiguration, followingValue: SampleConfiguration) -> Bool {
         return precedingValue.option == followingValue.option
     }
