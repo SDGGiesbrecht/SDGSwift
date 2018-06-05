bd58b63ccf440fc5fac8a363ab6256ba4c2fde34
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-06-04 18:21:10 -0600

    Manually implemented coding.

--- Sources/SampleConfiguration/SampleConfiguration.swift
+++ Sources/SampleConfiguration/SampleConfiguration.swift
@@ -21,4 +21,32 @@ public final class SampleConfiguration : Configuration {
 
     /// A customizable option.
     public var option: String = "Default"
+
+    // MARK: - Coding
+
+    private enum CodingKeys: CodingKey {
+        case option
+    }
+
+    public override func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(option, forKey: .option)
+
+        try super.encode(to: container.superEncoder())
+    }
+
+    public required init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        try super.init(from: container.superDecoder())
+
+        if let option = try container.decodeIfPresent(String.self, forKey: .option) {
+            self.option = option
+        }
+    }
+
+    // MARK: - Configuration
+
+    public required init() {
+        super.init()
+    }
 }
