01b82d2ff4ed145b83ce31e02ff17fb1c24be624
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-16 16:17:52 -0600

    Removed dead code.

--- Sources/SDGSwiftSource/TriviaPiece.swift
+++ Sources/SDGSwiftSource/TriviaPiece.swift
@@ -16,66 +16,6 @@ import SDGSwiftLocalizations
 
 extension TriviaPiece {
 
-    internal init(syntax: TriviaPieceSyntax) {
-        switch syntax.child {
-        case let token as ExtendedTokenSyntax:
-            switch token.kind {
-            case .whitespace, .newlines, .escape:
-                guard let first = token.text.first else {
-                    preconditionFailure(UserFacing<StrictString, APILocalization>({ localization in
-                        switch localization {
-                        case .englishCanada:
-                            return StrictString("Empty trivia.")
-                        }
-                    }))
-                }
-                switch first {
-                case " ":
-                    self = .spaces(token.text.count)
-                case "\u{9}":
-                    self = .tabs(token.text.count)
-                case "\u{A}":
-                    self = .newlines(token.text.count)
-                case "`":
-                    self = .backticks(token.text.count)
-                case "\u{B}":
-                    self = .verticalTabs(token.text.count)
-                case "\u{C}":
-                    self = .formfeeds(token.text.count)
-                default:
-                    preconditionFailure(UserFacing<StrictString, APILocalization>({ localization in
-                        switch localization {
-                        case .englishCanada:
-                            return StrictString("Invalid trivia characters: \(token.text)")
-                        }
-                    }))
-                }
-            default:
-                preconditionFailure(UserFacing<StrictString, APILocalization>({ localization in
-                    switch localization {
-                    case .englishCanada:
-                        return StrictString("Invalid trivia piece: \(token)")
-                    }
-                }))
-            }
-        case let documentation as LineDocumentationSyntax:
-            self = .docLineComment(documentation.text)
-        case let comment as LineCommentSyntax:
-            self = .lineComment(comment.text)
-        case let documentation as BlockDocumentationSyntax:
-            self = .blockComment(documentation.text)
-        case let comment as BlockCommentSyntax:
-            self = .blockComment(comment.text)
-        default:
-            preconditionFailure(UserFacing<StrictString, APILocalization>({ localization in
-                switch localization {
-                case .englishCanada:
-                    return StrictString("Invalid direct child of trivia piece: \(type(of: syntax.child))")
-                }
-            }))
-        }
-    }
-
     public var syntax: TriviaPieceSyntax {
         return TriviaPieceSyntax.parse(self)
     }
