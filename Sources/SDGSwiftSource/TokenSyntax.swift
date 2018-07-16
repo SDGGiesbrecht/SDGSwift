a4174d543669f03b2459761222cfcc173023ff2b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-15 23:34:20 -0600

    Detected closure signature.

--- Sources/SDGSwiftSource/TokenSyntax.swift
+++ Sources/SDGSwiftSource/TokenSyntax.swift
@@ -12,7 +12,10 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
 import SDGMathematics
+
+import SDGSwiftSyntaxShims
 
 extension TokenSyntax {
 
@@ -21,19 +24,21 @@ extension TokenSyntax {
     public var textFreedom: SyntaxElement.TextFreedom {
         switch tokenKind {
         case .identifier:
-            if text == "element" {
-                print(parent?.isDecl)
-                print(parent)
-            }
             if parent?.isDecl == true {
                 return .arbitrary
-            } else if indexInParent > 0,
-                (parent!.child(at: indexInParent − 1) as? TokenSyntax)?.tokenKind == .forKeyword {
-                // Declaration of loop variable.
-                return .arbitrary
-            } else {
-                return .aliasable
             }
+            if let parent = self.parent {
+                if (parent.child(at: indexInParent − 1) as? TokenSyntax)?.tokenKind == .forKeyword {
+                    // Loop variable declaration.
+                    return .arbitrary
+                }
+                if (parent.child(at: indexInParent + 1) as? TokenSyntax)?.tokenKind == .colon {
+                    // Closure argument declaration.
+                    return .arbitrary
+                }
+            }
+
+            return .aliasable
         case .unspacedBinaryOperator, .spacedBinaryOperator, .prefixOperator, .postfixOperator, .integerLiteral, .floatingLiteral, .stringLiteral:
             return .arbitrary
         case .unknown, .eof, .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword, .extensionKeyword, .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword, .operatorKeyword, .precedencegroupKeyword, .protocolKeyword, .structKeyword, .subscriptKeyword, .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword, .privateKeyword, .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword, .doKeyword, .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword, .returnKeyword, .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword, .caseKeyword, .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword, .falseKeyword, .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword, .capitalSelfKeyword, .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword, .__file__Keyword, .__line__Keyword, .__column__Keyword, .__function__Keyword, .__dso_handle__Keyword, .wildcardKeyword, .poundAvailableKeyword, .poundEndifKeyword, .poundElseKeyword, .poundElseifKeyword, .poundIfKeyword, .poundSourceLocationKeyword, .poundFileKeyword, .poundLineKeyword, .poundColumnKeyword, .poundFunctionKeyword, .arrow, .atSign, .colon, .semicolon, .comma, .period, .equal, .prefixPeriod, .leftParen, .rightParen, .leftBrace, .rightBrace, .leftSquareBracket, .rightSquareBracket, .leftAngle, .rightAngle, .ampersand, .postfixQuestionMark, .infixQuestionMark, .exclamationMark, .dollarIdentifier:
