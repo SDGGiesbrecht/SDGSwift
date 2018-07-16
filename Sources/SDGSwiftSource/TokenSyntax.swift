733a30b08ebf61ffa89a9c4a9f7eab87f7c1c3bf
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-15 23:35:33 -0600

    Handled operators.

--- Sources/SDGSwiftSource/TokenSyntax.swift
+++ Sources/SDGSwiftSource/TokenSyntax.swift
@@ -23,7 +23,7 @@ extension TokenSyntax {
 
     public var textFreedom: SyntaxElement.TextFreedom {
         switch tokenKind {
-        case .identifier:
+        case .identifier, .unspacedBinaryOperator, .spacedBinaryOperator, .prefixOperator, .postfixOperator:
             if parent?.isDecl == true {
                 return .arbitrary
             }
@@ -39,7 +39,7 @@ extension TokenSyntax {
             }
 
             return .aliasable
-        case .unspacedBinaryOperator, .spacedBinaryOperator, .prefixOperator, .postfixOperator, .integerLiteral, .floatingLiteral, .stringLiteral:
+        case .integerLiteral, .floatingLiteral, .stringLiteral:
             return .arbitrary
         case .unknown, .eof, .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword, .extensionKeyword, .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword, .operatorKeyword, .precedencegroupKeyword, .protocolKeyword, .structKeyword, .subscriptKeyword, .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword, .privateKeyword, .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword, .doKeyword, .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword, .returnKeyword, .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword, .caseKeyword, .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword, .falseKeyword, .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword, .capitalSelfKeyword, .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword, .__file__Keyword, .__line__Keyword, .__column__Keyword, .__function__Keyword, .__dso_handle__Keyword, .wildcardKeyword, .poundAvailableKeyword, .poundEndifKeyword, .poundElseKeyword, .poundElseifKeyword, .poundIfKeyword, .poundSourceLocationKeyword, .poundFileKeyword, .poundLineKeyword, .poundColumnKeyword, .poundFunctionKeyword, .arrow, .atSign, .colon, .semicolon, .comma, .period, .equal, .prefixPeriod, .leftParen, .rightParen, .leftBrace, .rightBrace, .leftSquareBracket, .rightSquareBracket, .leftAngle, .rightAngle, .ampersand, .postfixQuestionMark, .infixQuestionMark, .exclamationMark, .dollarIdentifier:
             return .invariable
