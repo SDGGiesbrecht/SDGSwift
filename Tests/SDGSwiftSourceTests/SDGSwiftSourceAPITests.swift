014cffcc5904954a044c99d33869d4cf23404b48
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-22 19:03:59 -0600

    Wiped out internal classes.

--- Tests/SDGSwiftSourceTests/SDGSwiftSourceAPITests.swift
+++ Tests/SDGSwiftSourceTests/SDGSwiftSourceAPITests.swift
@@ -119,12 +119,9 @@ class SDGSwiftSourceAPITests : TestCase {
 
                 TextFreedomHighlighter.targetTestFreedom = .invariable
                 try TextFreedomHighlighter().compare(syntax: sourceFile, parsedFrom: url, againstSpecification: "Invariable Text", overwriteSpecificationInsteadOfFailing: false)
-                /*
-                 #warning(Not handled yet.)
 
-                // API
-                let api = sourceFile.api(source: source).sorted().map({ $0.summary.joined(separator: "\n") }).joined(separator: "\n")
-                SDGPersistenceTestUtilities.compare(api, against: sourceDirectory.appendingPathComponent("After").appendingPathComponent("API").appendingPathComponent(url.deletingPathExtension().lastPathComponent).appendingPathExtension("txt"), overwriteSpecificationInsteadOfFailing: false)*/
+                let api = APIScanner().visit(sourceFile).source()
+                SDGPersistenceTestUtilities.compare(api, against: sourceDirectory.appendingPathComponent("After").appendingPathComponent("API").appendingPathComponent(url.deletingPathExtension().lastPathComponent).appendingPathExtension("txt"), overwriteSpecificationInsteadOfFailing: false)
             }
         } catch {
             XCTFail("\(error)")
