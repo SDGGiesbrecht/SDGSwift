6c8c1ba0b46140430e6b10a67916e657bf75b0bc
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-24 20:18:51 -0600

    Refactored variable detection.

--- Sources/SDGSwiftSource/Syntax/Syntax.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax.swift
@@ -70,7 +70,7 @@ extension Syntax {
         return Array(children.map({ $0.api() }).joined())
     }
 
-    private func isPublic() -> Bool {
+    internal func isPublic() -> Bool {
         return children.contains(where: { node in
             if let modifier = node as? DeclModifierSyntax,
                 modifier.name.tokenKind == .publicKeyword {
@@ -84,59 +84,13 @@ extension Syntax {
     /// Returns the API provided by this node.
     public func api() -> [APIElement] {
         switch self {
-        case is UnknownDeclSyntax :
+        case let unknown as UnknownDeclSyntax :
+            if unknown.isVariableSyntax {
+                return unknown.variableAPI.flatMap({ [$0] }) ?? []
+            }
             search: for child in children {
                 if let token = child as? TokenSyntax {
                     switch token.tokenKind {
-                    case .varKeyword, .letKeyword:
-                        // Variable
-                        if ¬isPublic() {
-                            return [] // Nothing nested is relevant.
-                        } else {
-                            if let nameToken = self.child(at: token.indexInParent + 1) as? TokenSyntax {
-                                switch nameToken.tokenKind {
-                                case .identifier(let name):
-                                    var typeName: String?
-                                    if let type = self.child(at: nameToken.indexInParent + 2) as? SimpleTypeIdentifierSyntax {
-                                        typeName = type.name.text
-                                    }
-                                    var isSettable = false
-                                    if token.tokenKind == .varKeyword {
-                                        if ¬children.contains(where: { node in
-                                            if let modifier = node as? DeclModifierSyntax {
-                                                switch modifier.name.tokenKind {
-                                                case .privateKeyword, .fileprivateKeyword, .internalKeyword:
-                                                    return true // Reduced setter access level.
-                                                default:
-                                                    break
-                                                }
-                                            }
-                                            return false
-                                        }) {
-                                            // No setter access reduction.
-
-                                            if children.contains(where: { node in
-                                                if let token = node as? TokenSyntax {
-                                                    switch token.tokenKind {
-                                                    case .unknown, // “set”
-                                                    .equal: // Stored property, not computed.
-                                                        return true
-                                                    default:
-                                                        break
-                                                    }
-                                                }
-                                                return false
-                                            }) {
-                                                isSettable = true
-                                            }
-                                        }
-                                    }
-                                    return [VariableAPI(name: name, type: typeName, isSettable: isSettable)]
-                                default:
-                                    break
-                                }
-                            }
-                        }
                     case .funcKeyword:
                         // Function
                         if ¬isPublic() {
