61821fd9b91d2fc391015e805cb5aae39788d702
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-07-25 19:38:44 -0600

    ...

--- Sources/SDGSwiftSource/API/APIElement.swift
+++ Sources/SDGSwiftSource/API/APIElement.swift
@@ -32,15 +32,14 @@ public class APIElement : Comparable {
 
     public static func < (precedingValue: APIElement, followingValue: APIElement) -> Bool {
         // #workaround(Swift 4.1.2, Order differs between operating systems.)
-        if precedingValue.name.scalars.elementsEqual(followingValue.name.scalars) {
-            return (precedingValue.declaration ?? "").scalars.lexicographicallyPrecedes((followingValue.declaration ?? "").scalars)
+        if precedingValue.name == followingValue.name {
+            return (precedingValue.declaration ?? "").scalars.lexicographicallyPrecedes((followingValue.declaration ?? "").scalars) // @exempt(from: tests) Empty declarations should never occur.
         } else {
             return precedingValue.name.scalars.lexicographicallyPrecedes(followingValue.name.scalars)
         }
     }
 
     public static func == (precedingValue: APIElement, followingValue: APIElement) -> Bool {
-        // #workaround(Swift 4.1.2, Order differs between operating systems.)
-        return precedingValue.name == followingValue.name
+        return (precedingValue.name, precedingValue.declaration) == (followingValue.name, followingValue.declaration)
     }
 }
