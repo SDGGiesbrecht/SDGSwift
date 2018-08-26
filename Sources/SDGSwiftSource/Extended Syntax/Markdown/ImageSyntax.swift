892d303e1c795d915b4728bff45a52f637d419b5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-08-26 16:26:51 -0600

    Handled images.

--- Sources/SDGSwiftSource/Extended Syntax/Markdown/ImageSyntax.swift
+++ Sources/SDGSwiftSource/Extended Syntax/Markdown/ImageSyntax.swift
@@ -12,18 +12,66 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGCollections
+
 import SDGCMarkShims
 
 public class ImageSyntax : MarkdownSyntax {
 
     internal init(node: cmark_node, in documentation: String) {
+
+        let imageDelimiter = ExtendedTokenSyntax(text: "!", kind: .imageDelimiter)
+        self.imageDelimiter = imageDelimiter
+
+        let openingDelimiter = ExtendedTokenSyntax(text: "[", kind: .linkDelimiter)
+        self.openingDelimiter = openingDelimiter
+
+        let medialDelimiter = ExtendedTokenSyntax(text: "](", kind: .linkDelimiter)
+        self.medialDelimiter = medialDelimiter
+
+        let url = ExtendedTokenSyntax(text: String(cString: cmark_node_get_url(node)), kind: .linkURL)
+        self.url = url
+
+        let closingDelimiter = ExtendedTokenSyntax(text: ")", kind: .linkDelimiter)
+        self.closingDelimiter = closingDelimiter
+
         super.init(node: node, in: documentation, precedingChildren: [
-            ExtendedTokenSyntax(text: "!", kind: .imageDelimiter),
-            ExtendedTokenSyntax(text: "[", kind: .linkDelimiter)
+            imageDelimiter,
+            openingDelimiter
             ], followingChildren: [
-                ExtendedTokenSyntax(text: "](", kind: .linkDelimiter),
-                ExtendedTokenSyntax(text: String(cString: cmark_node_get_url(node)), kind: .linkURL),
-                ExtendedTokenSyntax(text: ")", kind: .linkDelimiter)
+                medialDelimiter,
+                url,
+                closingDelimiter
             ])
+
+        let accountedFor: Set<ExtendedTokenKind> = [.imageDelimiter, .linkDelimiter, .linkURL]
+        contents = children.filter { child in
+            if let token = (child as? ExtendedTokenSyntax)?.kind,
+                token ∉ accountedFor {
+                return true
+            }
+            return false
+        }
+    }
+
+    // MARK: - Properties
+
+    public let imageDelimiter: ExtendedTokenSyntax
+
+    public let openingDelimiter: ExtendedTokenSyntax
+
+    public private(set) var contents: [ExtendedSyntax] = []
+
+    public let medialDelimiter: ExtendedTokenSyntax
+
+    public let url: ExtendedTokenSyntax
+
+    public let closingDelimiter: ExtendedTokenSyntax
+
+    // MARK: - ExtendedSyntax
+
+    public override func renderedHTML(internalIdentifiers: Set<String>) -> String {
+        let alternate = HTML.escape(contents.map({ $0.text }).joined())
+        return "<img alt=\u{22}" + alternate + "\u{22} src=\u{22}" + url.text + "\u{22}>"
     }
 }
