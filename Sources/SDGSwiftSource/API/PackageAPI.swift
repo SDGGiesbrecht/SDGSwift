c84ac993089272ab12f8525d7eded5791d31642e
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-26 16:09:26 +0300

    Fixed package documentation detection.

--- Sources/SDGSwiftSource/API/PackageAPI.swift
+++ Sources/SDGSwiftSource/API/PackageAPI.swift
@@ -50,7 +50,8 @@ public class PackageAPI : APIElement {
         }
         self.modules = modules
 
-        let declaration = manifest.smallestSubnode(containing: "Package(\n    name: \u{22}\(package.name)\u{22}")?.parent ?? manifest.smallestSubnode(containing: "Package(name: \u{22}\(package.name)\u{22}")?.parent // @exempt(from: tests) False coverage result in Xcode 9.4.1.
+        let node = (manifest.smallestSubnode(containing: "Package(\n    name: \u{22}\(package.name)\u{22}") ?? manifest.smallestSubnode(containing: "Package(name: \u{22}\(package.name)\u{22}")) // @exempt(from: tests) False coverage result in Xcode 9.4.1.
+        let declaration = node?.ancestors().first(where: { $0 is VariableDeclSyntax })
         super.init(documentation: declaration?.documentation)
     }
 
