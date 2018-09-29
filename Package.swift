a84b33dc2d8a6614221ae40fd9a12b8b48d1825b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-09-29 14:40:01 +0300

    Removed spaces from directory names to work around SR‐8803.

--- Package.swift
+++ Package.swift
@@ -67,8 +67,7 @@ let package = Package(
         .library(name: "SampleConfiguration", targets: ["SampleConfiguration"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 11, 0))),
-        // #warning(Pointing at branch.)
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 11, 1))),
         .package(url: "https://github.com/apple/swift\u{2D}package\u{2D}manager", .exact(Version(0, 3, 0))),
         .package(url: "https://github.com/apple/swift\u{2D}syntax", .exact(Version(0, 40200, 0)))
     ],
