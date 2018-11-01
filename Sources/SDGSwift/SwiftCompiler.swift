3a7e098bbcd46a56fb358622fdf923f311074c7d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-01 15:58:01 +0200

    Updated Swift version.

--- Sources/SDGSwift/SwiftCompiler.swift
+++ Sources/SDGSwift/SwiftCompiler.swift
@@ -24,7 +24,7 @@ public enum SwiftCompiler {
 
     // MARK: - Locating
 
-    internal static let versions = Version(4, 2, 0) /* Travis CI */ ... Version(4, 2, 0) /* Current */
+    internal static let versions = Version(4, 2, 0) /* Travis CI */ ... Version(4, 2, 1) /* Current */
 
     internal static let standardLocations = [
         // Swift
