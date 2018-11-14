4f660e2dbcfef2335f7f7292a100d14a185e8b9b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-14 17:01:28 +0100

    Fixed comma spacing.

--- Sources/SDGSwiftSource/Syntax/FunctionParameterSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/FunctionParameterSyntax.swift
@@ -47,15 +47,24 @@ extension FunctionParameterSyntax {
     }
 
     internal func normalizedForAssociatedValue() -> FunctionParameterSyntax {
+
+        // #workaround(SwiftSyntax 0.40200.0, SwiftSyntax puts the trailing comma here.)
+        let ellipsisToken: TokenSyntax?
+        if ellipsis?.tokenKind == .comma {
+            ellipsisToken = ellipsis?.generallyNormalized(trailingTrivia: .spaces(1))
+        } else {
+            ellipsisToken = ellipsis?.generallyNormalized()
+        }
+
         return SyntaxFactory.makeFunctionParameter(
             attributes: attributes?.normalizedForAPIDeclaration(),
             firstName: firstName?.generallyNormalized(trailingTrivia: .spaces(1)),
             secondName: secondName?.generallyNormalized(),
             colon: colon?.generallyNormalized(trailingTrivia: .spaces(1)),
             type: type?.normalized(),
-            ellipsis: ellipsis?.generallyNormalized(),
+            ellipsis: ellipsisToken,
             defaultArgument: defaultArgument?.normalizeForDefaultArgument(),
-            trailingComma: trailingComma?.generallyNormalized(leadingTrivia: .spaces(1)))
+            trailingComma: trailingComma?.generallyNormalized(trailingTrivia: .spaces(1)))
     }
 
     internal func forAssociatedValueName() -> FunctionParameterSyntax {
