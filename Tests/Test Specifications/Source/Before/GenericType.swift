bd74f5d60c331ee312b88ff6614a00b73d52c433
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-19 15:49:55 +0200

    ...

--- Tests/Test Specifications/Source/Before/GenericType.swift
+++ Tests/Test Specifications/Source/Before/GenericType.swift
@@ -8,4 +8,4 @@ public struct StandardFormGeneric<Thing> where Thing : Equatable {}
 public struct DeprecatedFormGeneric<Thing : Equatable> {}
 public struct DoubledDeprecatedFormGeneric<ThingA : Equatable, ThingB : Equatable> {}
 
-public struct NestedGenericConstraint<Thing> where Thing : Collection, Thing.Element : Equatable, Thing.Associated == Thing.Generic<Int> {}
+public struct NestedGenericConstraint<Thing> where Thing : Collection, Thing.Element : Equatable, Thing.Associated == Thing.Generic<Int>, OtherThing == Generic<Int> {}
