468ac3f8aef4e5b1b783433bce57a2fdbe399b58
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-11-23 14:03:38 +0200

    Fixed generic parameter list.

--- Sources/SDGSwiftSource/Syntax/GenericParameterListSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/GenericParameterListSyntax.swift
@@ -12,17 +12,24 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
+
 extension GenericParameterListSyntax {
 
     internal func normalizedForAPIDeclaration() -> (GenericParameterListSyntax, GenericRequirementListSyntax?) {
         var parameters: [GenericParameterSyntax] = []
         var requirements: [ConformanceRequirementSyntax] = []
         for parameter in self {
-            let (parameter, conformance) = parameter.normalizedForAPIDeclaration()
+            let (parameter, conformance) = parameter.normalizedForAPIDeclaration(comma: true)
             parameters.append(parameter)
             if let requirement = conformance {
                 requirements.append(requirement)
             }
+        }
+        if ¬parameters.isEmpty {
+            let last = parameters.removeLast()
+            let (withoutComma, _) = last.normalizedForAPIDeclaration(comma: false)
+            parameters.append(withoutComma)
         }
         return (SyntaxFactory.makeGenericParameterList(parameters),
                 requirements.isEmpty ? nil : SyntaxFactory.makeGenericRequirementList(requirements))
