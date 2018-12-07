48fd4619b1e5996b414d1e9d06b68dc879e6cd03
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-07 16:33:00 +0200

    Removed dead code.

--- Sources/SDGSwiftSource/SyntaxFactory.swift
+++ Sources/SDGSwiftSource/SyntaxFactory.swift
@@ -17,27 +17,4 @@ extension SyntaxFactory {
     public static func makeToken(_ kind: TokenKind, leadingTrivia: Trivia = [], trailingTrivia: Trivia = []) -> TokenSyntax {
         return SyntaxFactory.makeToken(kind, presence: .present, leadingTrivia: leadingTrivia, trailingTrivia: trailingTrivia)
     }
-
-    public static func makeProtocolStyleAccessorBlock(settable: Bool) -> AccessorBlockSyntax {
-        var list: [AccessorDeclSyntax] = [
-            SyntaxFactory.makeAccessorDecl(
-                attributes: nil,
-                modifier: nil,
-                accessorKind: SyntaxFactory.makeContextualKeyword("get"),
-                parameter: nil,
-                body: nil)
-        ]
-        if settable {
-            list.append(SyntaxFactory.makeAccessorDecl(
-                attributes: nil,
-                modifier: nil,
-                accessorKind: SyntaxFactory.makeToken(.contextualKeyword("set"), leadingTrivia: .spaces(1)),
-                parameter: nil,
-                body: nil))
-        }
-        return SyntaxFactory.makeAccessorBlock(
-            leftBrace: SyntaxFactory.makeToken(.leftBrace, leadingTrivia: .spaces(1), trailingTrivia: .spaces(1)),
-            accessorListOrStmtList: SyntaxFactory.makeAccessorList(list),
-            rightBrace: SyntaxFactory.makeToken(.rightBrace, leadingTrivia: .spaces(1)))
-    }
 }
