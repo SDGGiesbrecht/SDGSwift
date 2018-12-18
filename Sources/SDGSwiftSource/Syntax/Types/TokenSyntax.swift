a7ded42b8a7a5ed89ee1e61b4a99a403adf91887
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-18 15:03:12 -0600

    Added token locations.

--- Sources/SDGSwiftSource/Syntax/Types/TokenSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Types/TokenSyntax.swift
@@ -110,6 +110,84 @@ extension TokenSyntax {
         }
     }
 
+    // MARK: - Location
+
+    private func index(in string: String, for position: AbsolutePosition) -> String.ScalarView.Index {
+        let utf8 = string.utf8
+        return utf8.index(utf8.startIndex, offsetBy: position.utf8Offset)
+    }
+
+    public func lowerTriviaBound(in string: String) -> String.ScalarView.Index {
+        return index(in: string, for: position)
+    }
+
+    public func lowerTokenBound(in string: String) -> String.ScalarView.Index {
+        return index(in: string, for: positionAfterSkippingLeadingTrivia)
+    }
+
+    public func upperTokenBound(in string: String) -> String.ScalarView.Index {
+        return index(in: string, for: endPosition)
+    }
+
+    public func upperTriviaBound(in string: String) -> String.ScalarView.Index {
+        return index(in: string, for: endPositionAfterTrailingTrivia)
+    }
+
+    public func tokenRange(in string: String) -> Range<String.ScalarView.Index> {
+        return lowerTokenBound(in: string) ..< upperTokenBound(in: string)
+    }
+
+    public func triviaRange(in string: String) -> Range<String.ScalarView.Index> {
+        return lowerTriviaBound(in: string) ..< upperTriviaBound(in: string)
+    }
+
+    // MARK: - Syntax Tree
+
+    internal func firstPrecedingTrivia() -> TriviaPiece? {
+        return leadingTrivia.last() ?? previousToken()?.trailingTrivia.last()
+    }
+
+    internal func firstFollowingTrivia() -> TriviaPiece? {
+        return trailingTrivia.first ?? nextToken()?.leadingTrivia.first
+    }
+
+    internal func previousToken() -> TokenSyntax? {
+        func previousSibling(of relationship: (parent: Syntax, index: Int)) -> Syntax? {
+            var previousIndex = relationship.index
+            while previousIndex > 0 {
+                previousIndex −= 1
+                if let exists = relationship.parent.child(at: previousIndex) {
+                    return exists
+                }
+            }
+            return nil
+        }
+
+        let sharedAncestor = ancestorRelationships().first(where: { relationship in
+            if previousSibling(of: relationship) ≠ nil {
+                return true
+            }
+            return false
+        })
+
+        return sharedAncestor.flatMap({ previousSibling(of: $0) })?.lastToken()
+    }
+
+    internal func nextToken() -> TokenSyntax? {
+        func nextSibling(of relationship: (parent: Syntax, index: Int)) -> Syntax? {
+            return relationship.parent.child(at: relationship.index + 1)
+        }
+
+        let sharedAncestor = ancestorRelationships().first(where: { relationship in
+            if nextSibling(of: relationship) ≠ nil {
+                return true
+            }
+            return false
+        })
+
+        return sharedAncestor.flatMap({ nextSibling(of: $0) })?.firstToken()
+    }
+
     // MARK: - Syntax Highlighting
 
     internal func syntaxHighlightingClass(internalIdentifiers: Set<String>) -> String? {
