285c5e164fc3fe145f893e1bc9879a9022608da7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-21 15:33:11 -0600

    Added test.

--- Tests/Test Specifications/Source/Before/Operators.swift
+++ Tests/Test Specifications/Source/Before/Operators.swift
@@ -1,4 +1,7 @@
 extension Bool {
 
     public static func += (precedingValue: inout Bool, followingValue: Bool) {}
+    public static func − (_ explicitlyUnlabelledParameters: Int, _ b: Int) -> Int {
+        return b
+    }
 }
