e128ec34ffc2a7c5fcca507826369cba094aa8fe
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2018-12-23 17:25:46 -0600

    Fixed.

---
+++ Sources/SDGSwiftSource/Syntax/Types/CompositionTypeElementListSyntax.swift
@@ -0,0 +1,24 @@
+/*
+ CompositionTypeElementListSyntax.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+extension CompositionTypeElementListSyntax {
+
+    internal func normalized() -> CompositionTypeElementListSyntax {
+        var result = map({ $0.normalized(withAmpersand: true) }).sorted(by: { $0.source() < $1.source() })
+        if let last = result.indices.last {
+            result[last] = result[last].normalized(withAmpersand: false)
+        }
+        return SyntaxFactory.makeCompositionTypeElementList(result)
+    }
+}
---
+++ Sources/SDGSwiftSource/Syntax/Types/MetatypeTypeSyntax.swift
@@ -0,0 +1,23 @@
+/*
+ MetatypeTypeSyntax.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
+
+ Copyright ©2018 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+extension MetatypeTypeSyntax {
+
+    internal func normalized() -> MetatypeTypeSyntax {
+        return SyntaxFactory.makeMetatypeType(
+            baseType: baseType.normalized(),
+            period: period.generallyNormalizedAndMissingInsteadOfNil(),
+            typeOrProtocol: typeOrProtocol.generallyNormalizedAndMissingInsteadOfNil())
+    }
+}
