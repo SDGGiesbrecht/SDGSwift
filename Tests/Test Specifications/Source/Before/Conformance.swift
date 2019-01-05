a1dd51bdbcd57ff546d0411285bae47c907aaaab
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-01-04 22:00:38 -0600

    Added tests.

--- Tests/Test Specifications/Source/Before/Conformance.swift
+++ Tests/Test Specifications/Source/Before/Conformance.swift
@@ -7,6 +7,10 @@ public struct Conforming : CustomStringConvertible {
 
 public protocol BaseProtocol {
     func baseProtocolMethod()
+    associatedtype AssociatedType
+    init()
+    var protocolVariable: Int
+    subscript(protocolSubscript: Int) -> Int
 }
 public protocol MoreSpecificProtocol : BaseProtocol {
     func moreSpecificProtocolMethod()
@@ -14,4 +18,8 @@ public protocol MoreSpecificProtocol : BaseProtocol {
 public struct Structure : MoreSpecificProtocol {
     public func baseProtocolMethod()
     public func moreSpecificProtocolMethod()
+    public typealias AssociatedType = Int
+    public init() {}
+    public var protocolVariable: Int = 0
+    public subscript(protocolSubscript: Int) -> Int { return 0 }
 }
