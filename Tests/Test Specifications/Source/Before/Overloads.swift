ad5de476ea2d065dc457817b23a14ed2fc6206bd
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-01-14 17:06:46 -0600

    Added test.

---
+++ Tests/Test Specifications/Source/Before/Overloads.swift
@@ -0,0 +1,12 @@
+/// Does something with a string.
+public func overloadedFunction(_ string: String) {}
+
+/// Does something with a number.
+public func overloadedFunction(_ number: Int) {}
+
+extension Bool {
+    public func overloadedMethod(_ specific: Int) {}
+    /// Does something with a number.
+    public func overloadedMethod<T>(_ general: T) where T : Numeric {}
+    public func overloadedMethod<T>(_ general: T) where T : SignedNumeric {}
+}
