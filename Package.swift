6dc13ea098f2014791fb333e934bea1b44c1eb10
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-02-07 14:45:39 -0600

    ...

--- Package.swift
+++ Package.swift
@@ -35,7 +35,7 @@ let package = Package(
         // @documentation(SDGSwift)
         /// A basic interface for the Swift compiler.
         ///
-        /// This module includes development time tasks such as building and testing. It uses the command‐line interface and provides the command line output in real time.
+        /// This module includes development‐time tasks such as building and testing. It uses the command‐line interface and provides the command line output in real time.
         .library(name: "SDGSwift", targets: ["SDGSwift"]),
 
         // @documentation(SDGSwiftPackageManager)
@@ -46,6 +46,8 @@ let package = Package(
 
         // @documentation(SDGSwiftSource)
         /// Utilities for working with Swift source code.
+        ///
+        /// This module is built on SwiftSyntax and provides utilities such as syntax colouring and API parsing.
         .library(name: "SDGSwiftSource", targets: ["SDGSwiftSource"]),
 
         // @documentation(SDGXcode)
@@ -76,7 +78,7 @@ let package = Package(
         // #documentation(SDGSwift)
         /// A basic interface for the Swift compiler.
         ///
-        /// This module includes development time tasks such as building and testing. It uses the command‐line interface and provides the command line output in real time.
+        /// This module includes development‐time tasks such as building and testing. It uses the command‐line interface and provides the command line output in real time.
         .target(name: "SDGSwift", dependencies: [
             "SDGSwiftLocalizations",
             .product(name: "SDGControlFlow", package: "SDGCornerstone"),
@@ -99,6 +101,8 @@ let package = Package(
 
         // #documentation(SDGSwiftSource)
         /// Utilities for working with Swift source code.
+        ///
+        /// This module is built on SwiftSyntax and provides utilities such as syntax colouring and API parsing.
         .target(name: "SDGSwiftSource", dependencies: [
             "SDGSwift",
             "SDGSwiftPackageManager",
