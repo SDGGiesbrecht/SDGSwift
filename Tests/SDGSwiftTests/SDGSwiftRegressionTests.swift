92223ce70f4338c16e783a434998a0d8168a9d0a
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-20 16:35:06 -0600

    Refactored regression test.

--- Tests/SDGSwiftTests/SDGSwiftRegressionTests.swift
+++ Tests/SDGSwiftTests/SDGSwiftRegressionTests.swift
@@ -26,11 +26,11 @@ class SDGSwiftRegressionTests : TestCase {
         // Untracked.
 
         #if os(macOS) // Never was an issue on Linux.
-        let moved = FileManager.default.url(in: .temporary, at: "Moved")
-        defer { try? FileManager.default.removeItem(at: moved) }
-        withMockDynamicLinkedExecutable { mock in
-            XCTAssertEqual(try Package(url: mock.location).execute(.development, of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
-            XCTAssertEqual(try Package(url: mock.location).execute(.version(Version(1, 0, 0)), of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
+        FileManager.default.withTemporaryDirectory(appropriateFor: nil) { moved in
+            withMockDynamicLinkedExecutable { mock in
+                XCTAssertEqual(try Package(url: mock.location).execute(.development, of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
+                XCTAssertEqual(try Package(url: mock.location).execute(.version(Version(1, 0, 0)), of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
+            }
         }
         #endif
     }
