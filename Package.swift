1658b4c0919d3671c549d9d5871ab41514d71e10
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-03-28 18:47:16 -0600

    Updated SDGCornerstone.

--- Package.swift
+++ Package.swift
@@ -70,7 +70,7 @@ let package = Package(
         .library(name: "SampleConfiguration", targets: ["SampleConfiguration"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .branch("master")),//.upToNextMinor(from: Version(0, 15, 0))),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", .upToNextMinor(from: Version(0, 15, 0))),
         .package(url: "https://github.com/apple/swift\u{2D}package\u{2D}manager", .exact(Version(0, 3, 0))),
         .package(url: "https://github.com/apple/swift\u{2D}syntax", .exact(Version(0, 50000, 0)))
     ],
