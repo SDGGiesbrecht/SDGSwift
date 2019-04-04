4246fe8e99e663361c28f66a7dd5f9672fc63873
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-04 13:25:18 -0600

    Regenerated test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,33 +1,20 @@
-/*
- LinuxMain.swift
-
- This source file is part of the SDGSwift open source project.
- https://sdggiesbrecht.github.io/SDGSwift
-
- Copyright ©2018–2019 Jeremy David Giesbrecht and the SDGSwift project contributors.
-
- Soli Deo gloria.
-
- Licensed under the Apache Licence, Version 2.0.
- See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
- */
-
 import XCTest
 
-import SDGSwiftDocumentationExampleTests
-import SDGSwiftTests
 import SDGSwiftConfigurationTests
+import SDGSwiftDocumentationExampleTests
 import SDGSwiftPackageManagerTests
 import SDGSwiftSourceTests
-import SDGXcodeTests
+import SDGSwiftTests
 import SDGXCTestUtilities
+import SDGXcodeTests
 
 var tests = [XCTestCaseEntry]()
-tests += SDGSwiftDocumentationExampleTests.__allTests()
-tests += SDGSwiftTests.__allTests()
 tests += SDGSwiftConfigurationTests.__allTests()
+tests += SDGSwiftDocumentationExampleTests.__allTests()
 tests += SDGSwiftPackageManagerTests.__allTests()
 tests += SDGSwiftSourceTests.__allTests()
+tests += SDGSwiftTests.__allTests()
+tests += SDGXCTestUtilities.__allTests()
 tests += SDGXcodeTests.__allTests()
 
 XCTMain(tests)
