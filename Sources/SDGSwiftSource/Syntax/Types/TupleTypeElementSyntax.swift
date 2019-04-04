435dee141bf82a768605d3cba4e9e6670d0da317
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-04-04 12:45:16 -0600

    Updated workaround.

--- Sources/SDGSwiftSource/Syntax/Types/TupleTypeElementSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Types/TupleTypeElementSyntax.swift
@@ -12,6 +12,8 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
+import SDGLogic
+
 extension TupleTypeElementSyntax {
 
     internal func normalized() -> TupleTypeElementSyntax {
@@ -24,8 +26,11 @@ extension TupleTypeElementSyntax {
             ellipsisToken = ellipsis?.generallyNormalized()
         }
 
-        // #workaround(SwiftSyntax 0.40200.0, Prevents invalid index use by SwiftSyntax.)
-        let newInitializer = source().contains("=") ? initializer?.normalizeForDefaultArgument() : nil // @exempt(from: tests) Unknown where this would be present.
+        // #workaround(SwiftSyntax 0.50000.0, Prevents invalid index use by SwiftSyntax.)
+        var initializer = self.initializer
+        if ¬source().contains("=") {
+            initializer = nil
+        }
 
         return SyntaxFactory.makeTupleTypeElement(
             inOut: inOut?.generallyNormalized(trailingTrivia: .spaces(1)),
@@ -34,7 +39,7 @@ extension TupleTypeElementSyntax {
             colon: colon?.generallyNormalized(trailingTrivia: .spaces(1)),
             type: type.normalized(),
             ellipsis: ellipsisToken,
-            initializer: newInitializer,
+            initializer: initializer?.normalizeForDefaultArgument(),
             trailingComma: trailingComma?.generallyNormalized(trailingTrivia: .spaces(1)))
     }
 }
