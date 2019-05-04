2d14659e9c66b51949c678f923ea7af6b69b33d4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-03 23:13:06 -0600

    Optimized documentation.

--- Workspace.swift
+++ Workspace.swift
@@ -54,6 +54,51 @@ configuration.repository.ignoredPaths.insert("Sources/Shims")
 configuration.testing.exemptPaths.insert("Sources/Shims")
 configuration.repository.ignoredPaths.insert("Tests/Test Specifications/Source")
 
-// #workaround(workspace version 0.19.0, Takes too long.)
-configuration.documentation.api.generate = false
-configuration.documentation.api.enforceCoverage = false
+configuration.documentation.api.ignoredDependencies = [
+
+    // SDGCornerstone
+    "SDGCalendar",
+    "SDGCollections",
+    "SDGCornerstoneLocalizations",
+    "SDGExternalProcess",
+    "SDGLocalizationTestUtilities",
+    "SDGLogic",
+    "SDGLogicTestUtilities",
+    "SDGMathematics",
+    "SDGPersistence",
+    "SDGPersistenceTestUtilities",
+    "SDGTesting",
+    "SDGText",
+    "SDGXCTestUtilities",
+
+    // llbuild
+    "libllbuild",
+    "llbuildBasic",
+    "llbuildBuildSystem",
+    "llbuildCore",
+    "llbuildSwift",
+    "llvmSupport",
+
+    // Swift
+    "Dispatch",
+    "Foundation",
+    "XCTest",
+
+    // SwiftPM
+    "Basic",
+    "Build",
+    "clibc",
+    "PackageGraph",
+    "PackageLoading",
+    "PackageModel",
+    "POSIX",
+    "SourceControl",
+    "SPMLLBuild",
+    "SPMLibc",
+    "SPMUtility",
+    "Workspace",
+    "Xcodeproj",
+
+    // SwiftSyntax
+    "SwiftSyntax"
+]
