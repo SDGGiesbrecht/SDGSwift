babb8f951783577d7e48c915f8f93fac4a0c0d3d
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-05 17:41:50 -0600

    Activated Linux test.

--- Tests/SDGSwiftTests/SDGSwiftRegressionTests.swift
+++ Tests/SDGSwiftTests/SDGSwiftRegressionTests.swift
@@ -38,13 +38,11 @@ class SDGSwiftRegressionTests : TestCase {
     func testDynamicLinking() throws {
         // Untracked.
 
-        #if os(macOS) // Never was an issue on Linux.
         try FileManager.default.withTemporaryDirectory(appropriateFor: nil) { moved in
             try withMockDynamicLinkedExecutable { mock in
                 XCTAssertEqual(try Package(url: mock.location).execute(.development, of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
                 XCTAssertEqual(try Package(url: mock.location).execute(.version(Version(1, 0, 0)), of: ["tool"], with: [], cacheDirectory: moved), "Hello, world!")
             }
         }
-        #endif
     }
 }
