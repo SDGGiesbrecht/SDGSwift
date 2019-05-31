f32341705484cf3823ba43459e07af5c8e24c990
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-05-30 18:14:26 -0600

    Added symbol documentation.

---
+++ Sources/SDGSwiftSource/API/SymbolDocumentation.swift
@@ -0,0 +1,24 @@
+/*
+ SymbolDocumentation.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+/// Symbol documentation.
+public struct SymbolDocumentation {
+
+    /// Any developer line comments preceding the documentation.
+    ///
+    /// These are included for use by custom tools that wish to extend the documentation functionality directly supported by Swift.
+    public let developerComments: [LineCommentSyntax]
+    /// The documentation.
+    public let documentationComment: DocumentationSyntax
+}
