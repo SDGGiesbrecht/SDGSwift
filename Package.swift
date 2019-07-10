8474acad5143e5dbd7bf71a03c7bdb8ff1943609
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-07-09 22:24:10 -0600

    Version 0.12.3

--- Package.swift
+++ Package.swift
@@ -83,11 +83,11 @@ let package = Package(
         .library(name: "SampleConfiguration", targets: ["SampleConfiguration"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(1, 0, 0)),
+        .package(url: "https://github.com/SDGGiesbrecht/SDGCornerstone", from: Version(2, 0, 0)),
         .package(url: "https://github.com/apple/swift\u{2D}package\u{2D}manager", .exact(Version(0, 4, 0))),
         .package(url: "https://github.com/apple/swift\u{2D}syntax", .exact(Version(0, 50000, 0))),
         .package(url: "https://github.com/SDGGiesbrecht/swift\u{2D}cmark", .exact(Version(0, 0, 50000))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(1, 0, 0))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(1, 0, 1))
     ],
     targets: [
 
