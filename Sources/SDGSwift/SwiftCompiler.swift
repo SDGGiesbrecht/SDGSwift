c0e3a43519fae54b68de46ec925655f38a3ad669
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-21 20:48:08 -0600

    Updated Swift.

--- Sources/SDGSwift/SwiftCompiler.swift
+++ Sources/SDGSwift/SwiftCompiler.swift
@@ -24,7 +24,7 @@ public enum SwiftCompiler {
 
     // MARK: - Locating
 
-    internal static let compatibleVersionRange = Version(5, 0, 0) /* Travis CI */ ... Version(5, 0, 1) /* Current */
+    internal static let compatibleVersionRange = Version(5, 1, 0) /* Travis CI */ ... Version(5, 1, 0) /* Current */
     public static let _standardLibraryVersion = compatibleVersionRange.lowerBound
 
     internal static let searchCommands: [[String]] = [
