361e8ace5eaf800c700b121ea204a7560a5409fa
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-22 13:17:00 -0600

    Updated SDGWeb.

--- Package.swift
+++ Package.swift
@@ -87,7 +87,7 @@ let package = Package(
         .package(url: "https://github.com/SDGGiesbrecht/swift\u{2D}package\u{2D}manager", .branch("swift‐5.1")),
         .package(url: "https://github.com/apple/swift\u{2D}syntax", .exact(Version(0, 50000, 0))),
         .package(url: "https://github.com/SDGGiesbrecht/swift\u{2D}cmark", .exact(Version(0, 0, 50000))),
-        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(2, 0, 0))
+        .package(url: "https://github.com/SDGGiesbrecht/SDGWeb", from: Version(3, 0, 0))
     ],
     targets: [
 
