e06a4d62194913b2e6d78909c18cbd9fb81185af
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-09-22 15:14:58 -0600

    Filled in missing sequence method.

---
+++ Sources/SDGSwiftSource/Sequence.swift
@@ -0,0 +1,20 @@
+/*
+ SyntaxFactory.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+extension Sequence {
+
+    internal var first: Element? {
+        return first(where: { _ in true })
+    }
+}
