132a7165cbab8b4533b1f7afb3c3018fa491fea2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-10-04 19:39:21 -0600

    Bumped version.

--- Workspace.swift
+++ Workspace.swift
@@ -17,7 +17,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(0, 13, 1)
+configuration.documentation.currentVersion = Version(0, 14, 0)
 
 configuration.documentation.projectWebsite = URL(string: "https://sdggiesbrecht.github.io/SDGSwift")!
 configuration.documentation.documentationURL = URL(string: "https://sdggiesbrecht.github.io/SDGSwift")!
