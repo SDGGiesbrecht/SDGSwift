8df23d36bdaa7ce54110596cbd879c82ee8f6f20
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-10-26 16:35:49 -0600

    Updated Workspace and Xcode.

--- Sources/SDGXcode/Xcode.swift
+++ Sources/SDGXcode/Xcode.swift
@@ -31,7 +31,7 @@ public enum Xcode {
 
     // MARK: - Locating
 
-    internal static let compatibleVersionRange = Version(11, 0, 0) /* Travis CI */ ... Version(11, 1, 0) /* Current */
+    internal static let compatibleVersionRange = Version(11, 1, 0) /* Travis CI */ ... Version(11, 1, 0) /* Current */
 
     internal static let searchCommands: [[String]] = [
         ["xcrun", "\u{2D}\u{2D}find", "xcodebuild"] // Xcode
