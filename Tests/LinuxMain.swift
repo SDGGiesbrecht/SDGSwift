f2bc71e14c19c722dff1e211c7ca6695dce5befe
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-05 22:57:42 -0600

    Removed test manifests.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -11,22 +11,3 @@
  Licensed under the Apache Licence, Version 2.0.
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
-
-import XCTest
-
-import SDGSwiftConfigurationTests
-import SDGSwiftDocumentationExampleTests
-import SDGSwiftPackageManagerTests
-import SDGSwiftSourceTests
-import SDGSwiftTests
-import SDGXcodeTests
-
-var tests = [XCTestCaseEntry]()
-tests += SDGSwiftConfigurationTests.__allTests()
-tests += SDGSwiftDocumentationExampleTests.__allTests()
-tests += SDGSwiftPackageManagerTests.__allTests()
-tests += SDGSwiftSourceTests.__allTests()
-tests += SDGSwiftTests.__allTests()
-tests += SDGXcodeTests.__allTests()
-
-XCTMain(tests)
