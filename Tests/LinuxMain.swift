266bd858313ad8d019f532eede35d3b764b95f28
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-11-06 13:58:24 -0600

    Updated Swift and Xcode.

--- Tests/LinuxMain.swift
+++ Tests/LinuxMain.swift
@@ -1,18 +1,13 @@
-import XCTest
+/*
+ LinuxMain.swift
 
-import SDGSwiftConfigurationTests
-import SDGSwiftDocumentationExampleTests
-import SDGSwiftPackageManagerTests
-import SDGSwiftSourceTests
-import SDGSwiftTests
-import SDGXcodeTests
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
 
-var tests = [XCTestCaseEntry]()
-tests += SDGSwiftConfigurationTests.__allTests()
-tests += SDGSwiftDocumentationExampleTests.__allTests()
-tests += SDGSwiftPackageManagerTests.__allTests()
-tests += SDGSwiftSourceTests.__allTests()
-tests += SDGSwiftTests.__allTests()
-tests += SDGXcodeTests.__allTests()
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGSwift project contributors.
 
-XCTMain(tests)
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
