9e4d8c5745dd1ceb17e023730e2a48ee89df87f6
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2019-12-24 00:04:24 -0600

    Improved scheme detection.

---
+++ Tests/Mock Projects/MultipleSchemes/Tests/SomeTests/File.swift
@@ -0,0 +1,23 @@
+/*
+ File.swift
+
+ This source file is part of the SDGSwift open source project.
+ https://sdggiesbrecht.github.io/SDGSwift
+
+ Copyright ©2019 Jeremy David Giesbrecht and the SDGSwift project contributors.
+
+ Soli Deo gloria.
+
+ Licensed under the Apache Licence, Version 2.0.
+ See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
+ */
+
+import SomeLibrary
+
+import XCTest
+
+final class SomeTests: XCTestCase {
+  func testLibrary() {
+    someFunction()
+  }
+}
