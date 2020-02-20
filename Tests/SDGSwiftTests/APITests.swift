50f0b09457ece42eeb7762a8322db76464d5d6d2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-20 16:30:41 -0600

    Disabled test.

--- Tests/SDGSwiftTests/APITests.swift
+++ Tests/SDGSwiftTests/APITests.swift
@@ -266,24 +266,26 @@ class SDGSwiftAPITests: SDGSwiftTestUtilities.TestCase {
   }
 
   func testVersion() {
-    testCustomStringConvertibleConformance(
-      of: Version(1, 2, 3),
-      localizations: InterfaceLocalization.self,
-      uniqueTestName: "1.2.3",
-      overwriteSpecificationInsteadOfFailing: false
-    )
+    #if !os(Windows)  // #workaround(Swift 5.1.3, SegFault)
+      testCustomStringConvertibleConformance(
+        of: Version(1, 2, 3),
+        localizations: InterfaceLocalization.self,
+        uniqueTestName: "1.2.3",
+        overwriteSpecificationInsteadOfFailing: false
+      )
 
-    XCTAssertEqual(Version(firstIn: "1.0.0"), Version(1, 0, 0))
-    XCTAssertEqual(Version(firstIn: "1.0"), Version(1, 0, 0))
-    XCTAssertEqual(Version(firstIn: "1"), Version(1, 0, 0))
-    XCTAssertNil(Version(String("Blah blah blah...")))
-    XCTAssertNil(Version(firstIn: "Blah blah blah..."))
-    XCTAssertNil(Version(String("1.0.0.0")))
-    XCTAssertNil(Version(String("1.0.A")))
-    XCTAssertNil(Version(String("1.A")))
-    XCTAssertNil(Version(String("A")))
-    XCTAssertEqual(Version(0, 1, 0).compatibleVersions.upperBound, Version(0, 2, 0))
-    XCTAssertEqual(Version(1, 0, 0), "1.0.0")
+      XCTAssertEqual(Version(firstIn: "1.0.0"), Version(1, 0, 0))
+      XCTAssertEqual(Version(firstIn: "1.0"), Version(1, 0, 0))
+      XCTAssertEqual(Version(firstIn: "1"), Version(1, 0, 0))
+      XCTAssertNil(Version(String("Blah blah blah...")))
+      XCTAssertNil(Version(firstIn: "Blah blah blah..."))
+      XCTAssertNil(Version(String("1.0.0.0")))
+      XCTAssertNil(Version(String("1.0.A")))
+      XCTAssertNil(Version(String("1.A")))
+      XCTAssertNil(Version(String("A")))
+      XCTAssertEqual(Version(0, 1, 0).compatibleVersions.upperBound, Version(0, 2, 0))
+      XCTAssertEqual(Version(1, 0, 0), "1.0.0")
+    #endif
   }
 
   func testVersionedExternalProcess() {
