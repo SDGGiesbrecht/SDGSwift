cb4de92c793be6cb0b761eb467dcda634a803045
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-02-19 22:23:54 -0600

    Disabled more.

--- Tests/SDGSwiftSourceTests/RegressionTests.swift
+++ Tests/SDGSwiftSourceTests/RegressionTests.swift
@@ -12,11 +12,11 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-import SwiftSyntax
-
 #if (os(Windows) || os(Android))  // #workaround(Swift 5.1.3, SwiftSyntax won’t compile.)
-  import SDGSwiftSource
+  import SwiftSyntax
 #endif
+
+import SDGSwiftSource
 
 import XCTest
 
