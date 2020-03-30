71163e9fb9662421ca7465952f1bb939b119f78b
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-03-30 13:30:51 -0600

    Updated TokenSyntax.

--- Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/TokenSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/TokenSyntax.swift
@@ -41,53 +41,53 @@
       case .identifier, .unspacedBinaryOperator, .spacedBinaryOperator, .prefixOperator,
         .postfixOperator:
         if let parent = self.parent {
-          if let enumerationCaseElement = parent as? EnumCaseElementSyntax,
+          if let enumerationCaseElement = parent.as(EnumCaseElementSyntax.self),
             enumerationCaseElement.identifier == self
           {
             // Enumeration case declaration.
             return .arbitrary
           }
-          if let identifierPattern = parent as? IdentifierPatternSyntax,
+          if let identifierPattern = parent.as(IdentifierPatternSyntax.self),
             identifierPattern.identifier == self
           {
             // Variable declaration.
             return .arbitrary
           }
-          if let functionParameter = parent as? FunctionParameterSyntax,
+          if let functionParameter = parent.as(FunctionParameterSyntax.self),
             functionParameter.firstName == self ∨ functionParameter.secondName == self
           {
             // Function parameter declaration.
             return .arbitrary
           }
-          if let genericParameter = parent as? GenericParameterSyntax,
+          if let genericParameter = parent.as(GenericParameterSyntax.self),
             genericParameter.name == self
           {
             // Generic member type declaration.
             return .arbitrary
           }
 
-          if let attribute = parent as? AttributeSyntax,
+          if let attribute = parent.as(AttributeSyntax.self),
             attribute.attributeName == self
           {
             return .invariable
           }
-          if let declarationModifier = parent as? DeclModifierSyntax,
+          if let declarationModifier = parent.as(DeclModifierSyntax.self),
             declarationModifier.name == self
           {
             // “open”, “mutating”, etc.
             return .invariable
           }
-          if let accessPath = parent as? AccessPathComponentSyntax,
+          if let accessPath = parent.as(AccessPathComponentSyntax.self),
             accessPath.name == self
           {
             // Import statement.
             return .invariable
           }
-          var previousAncestor: Syntax = self
+          var previousAncestor: Syntax = Syntax(self)
           for ancestor in ancestors() {
             defer { previousAncestor = ancestor }
-            if let ifConfigurationClause = ancestor as? IfConfigClauseSyntax,
-              let condition = ifConfigurationClause.condition,
+            if let ifConfigurationClause = ancestor.as(IfConfigClauseSyntax.self),
+              let condition = Syntax(ifConfigurationClause.condition),
               condition == previousAncestor
             {
               // #if condition
@@ -95,11 +95,13 @@
             }
           }
 
-          if parent.isDecl == true {
+          if parent.isProtocol(DeclSyntaxProtocol.self) == true {
             /// Declaration.
             return .arbitrary
           }
-          if let argument = parent as? FunctionCallArgumentSyntax,
+          if let argument = parent.as(TupleExprElementSyntax.self),
+            let argumentList = argument.parent?.as(TupleExprElementListSyntax.self),
+            argumentList.parent?.is(FunctionCallExprSyntax.self) == true,
             argument.label == self
           {
             return .aliasable
@@ -112,29 +114,7 @@
         return .aliasable
       case .stringSegment, .integerLiteral, .floatingLiteral, .stringLiteral:
         return .arbitrary
-      case .eof, .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword,
-        .extensionKeyword, .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword,
-        .operatorKeyword, .precedencegroupKeyword, .protocolKeyword, .structKeyword,
-        .subscriptKeyword, .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword,
-        .privateKeyword, .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword,
-        .doKeyword, .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword,
-        .returnKeyword, .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword,
-        .caseKeyword, .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword,
-        .falseKeyword, .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword,
-        .capitalSelfKeyword, .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword,
-        .__file__Keyword, .__line__Keyword, .__column__Keyword, .__function__Keyword,
-        .__dso_handle__Keyword, .wildcardKeyword, .poundAvailableKeyword, .poundEndifKeyword,
-        .poundElseKeyword, .poundElseifKeyword, .poundIfKeyword, .poundSourceLocationKeyword,
-        .poundFileKeyword, .poundLineKeyword, .poundColumnKeyword, .poundDsohandleKeyword,
-        .poundFunctionKeyword, .poundSelectorKeyword, .poundKeyPathKeyword,
-        .poundColorLiteralKeyword,
-        .poundFileLiteralKeyword, .poundImageLiteralKeyword, .arrow, .atSign, .colon, .semicolon,
-        .comma, .period, .equal, .prefixPeriod, .leftParen, .rightParen, .leftBrace, .rightBrace,
-        .leftSquareBracket, .rightSquareBracket, .leftAngle, .rightAngle, .prefixAmpersand,
-        .postfixQuestionMark, .infixQuestionMark, .exclamationMark, .backslash,
-        .stringInterpolationAnchor, .stringQuote, .multilineStringQuote, .dollarIdentifier,
-        .contextualKeyword, .unknown, .pound, .backtick, .poundAssertKeyword, .poundWarningKeyword,
-        .poundErrorKeyword, .yield, .ellipsis, .singleQuote, .rawStringDelimiter:
+      case .eof, .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword, .extensionKeyword, .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword, .operatorKeyword, .precedencegroupKeyword, .protocolKeyword, .structKeyword, .subscriptKeyword, .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword, .privateKeyword, .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword, .doKeyword, .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword, .returnKeyword, .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword, .caseKeyword, .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword, .falseKeyword, .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword, .capitalSelfKeyword, .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword, .__file__Keyword, .__line__Keyword, .__column__Keyword, .__function__Keyword, .__dso_handle__Keyword, .wildcardKeyword, .poundAvailableKeyword, .poundEndifKeyword, .poundElseKeyword, .poundElseifKeyword, .poundIfKeyword, .poundSourceLocationKeyword, .poundFileKeyword, .poundFilePathKeyword, .poundLineKeyword, .poundColumnKeyword, .poundDsohandleKeyword, .poundFunctionKeyword, .poundSelectorKeyword, .poundKeyPathKeyword, .poundColorLiteralKeyword, .poundFileLiteralKeyword, .poundImageLiteralKeyword, .arrow, .atSign, .colon, .semicolon, .comma, .period, .equal, .prefixPeriod, .leftParen, .rightParen, .leftBrace, .rightBrace, .leftSquareBracket, .rightSquareBracket, .leftAngle, .rightAngle, .prefixAmpersand, .postfixQuestionMark, .infixQuestionMark, .exclamationMark, .backslash, .stringInterpolationAnchor, .stringQuote, .multilineStringQuote, .dollarIdentifier, .contextualKeyword, .unknown, .pound, .backtick, .poundAssertKeyword, .poundWarningKeyword, .poundErrorKeyword, .yield, .ellipsis, .singleQuote, .rawStringDelimiter:
         return .invariable
       }
     }
@@ -205,22 +185,7 @@
       case .eof, .unknown:
         return nil
 
-      case .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword, .extensionKeyword,
-        .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword, .operatorKeyword,
-        .precedencegroupKeyword, .protocolKeyword, .structKeyword, .subscriptKeyword,
-        .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword, .privateKeyword,
-        .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword, .doKeyword,
-        .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword, .returnKeyword,
-        .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword, .caseKeyword,
-        .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword, .falseKeyword,
-        .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword, .capitalSelfKeyword,
-        .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword, .__file__Keyword,
-        .__line__Keyword,
-        .__column__Keyword, .__function__Keyword, .__dso_handle__Keyword, .wildcardKeyword,
-        .poundAvailableKeyword, .poundSourceLocationKeyword, .poundFileKeyword, .poundLineKeyword,
-        .poundColumnKeyword, .poundDsohandleKeyword, .poundFunctionKeyword, .poundSelectorKeyword,
-        .poundKeyPathKeyword, .poundColorLiteralKeyword, .poundFileLiteralKeyword,
-        .poundImageLiteralKeyword, .atSign, .contextualKeyword, .poundAssertKeyword, .yield:
+      case .associatedtypeKeyword, .classKeyword, .deinitKeyword, .enumKeyword, .extensionKeyword, .funcKeyword, .importKeyword, .initKeyword, .inoutKeyword, .letKeyword, .operatorKeyword, .precedencegroupKeyword, .protocolKeyword, .structKeyword, .subscriptKeyword, .typealiasKeyword, .varKeyword, .fileprivateKeyword, .internalKeyword, .privateKeyword, .publicKeyword, .staticKeyword, .deferKeyword, .ifKeyword, .guardKeyword, .doKeyword, .repeatKeyword, .elseKeyword, .forKeyword, .inKeyword, .whileKeyword, .returnKeyword, .breakKeyword, .continueKeyword, .fallthroughKeyword, .switchKeyword, .caseKeyword, .defaultKeyword, .whereKeyword, .catchKeyword, .asKeyword, .anyKeyword, .falseKeyword, .isKeyword, .nilKeyword, .rethrowsKeyword, .superKeyword, .selfKeyword, .capitalSelfKeyword, .throwKeyword, .trueKeyword, .tryKeyword, .throwsKeyword, .__file__Keyword, .__line__Keyword, .__column__Keyword, .__function__Keyword, .__dso_handle__Keyword, .wildcardKeyword, .poundAvailableKeyword, .poundSourceLocationKeyword, .poundFileKeyword, .poundFilePathKeyword, .poundLineKeyword, .poundColumnKeyword, .poundDsohandleKeyword, .poundFunctionKeyword, .poundSelectorKeyword, .poundKeyPathKeyword, .poundColorLiteralKeyword, .poundFileLiteralKeyword, .poundImageLiteralKeyword, .atSign, .contextualKeyword, .poundAssertKeyword, .yield:
         return "keyword"
 
       case .poundEndifKeyword, .poundElseKeyword, .poundElseifKeyword, .poundIfKeyword, .pound,
@@ -239,13 +204,13 @@
         if name == "get" ∨ name == "set" {
           return "keyword"
         }
-        if let attribute = parent as? AttributeSyntax,
+        if let attribute = parent?.as(AttributeSyntax.self),
           attribute.attributeName == self
         {
           // @available, @objc, etc.
           return "keyword"
         }
-        if let declarationModifier = parent as? DeclModifierSyntax,
+        if let declarationModifier = parent?.as(DeclModifierSyntax.self),
           declarationModifier.name == self
         {
           // “open”, “mutating”, etc.
