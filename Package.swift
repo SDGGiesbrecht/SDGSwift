0b168d6b39d46ab64c9592828f89b580c8a773b2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-10-22 15:15:45 -0600

    Tried conditional compilation for Android.

--- Package.swift
+++ Package.swift
@@ -148,7 +148,8 @@ let package = Package(
           name: "SwiftPM\u{2D}auto",
           package: "SwiftPM",
           // #workaround(SwiftPM 0.50300.0, Does not support Windows yet.)
-          condition: .when(platforms: [.macOS, .wasi, .linux, .android])
+          // #workaround(SwiftPM 0.50300.0, Does not support Andriod yet.)
+          condition: .when(platforms: [.macOS, .wasi, .linux])
         ),
       ]
     ),
