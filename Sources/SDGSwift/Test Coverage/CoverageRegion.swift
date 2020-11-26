5139b3b3d271ae18d8c0b961944810b35846abe5
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2020-11-26 16:59:38 -0600

    Fixed web?

--- Sources/SDGSwift/Test Coverage/CoverageRegion.swift
+++ Sources/SDGSwift/Test Coverage/CoverageRegion.swift
@@ -12,9 +12,7 @@
  See http://www.apache.org/licenses/LICENSE-2.0 for licence information.
  */
 
-#if !os(WASI)  // #workaround(Swift 5.3, Web lacks Foundation.)
-  import Foundation
-#endif
+import Foundation
 
 import SDGLogic
 import SDGCollections
