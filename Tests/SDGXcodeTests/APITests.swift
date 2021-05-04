4030fd7c652bcb4a2a6a28ea4e5d89413ff59e2f
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-05-03 20:01:26 -0600

    ...

--- Tests/SDGXcodeTests/APITests.swift
+++ Tests/SDGXcodeTests/APITests.swift
@@ -286,6 +286,8 @@ class APITests: SDGSwiftTestUtilities.TestCase {
               filtered = filtered.filter({ ¬$0.contains("Using new build system") })
               filtered = filtered.filter({ ¬$0.contains("unable to get a dev_t") })
               filtered = filtered.filter({ ¬$0.contains("XCTHTestRunSpecification") })
+              filtered = filtered.filter({ $0 ≠ "(" })
+              filtered = filtered.filter({ $0 ≠ ")" })
               #if PLATFORM_HAS_XCODE
                 compare(
                   filtered.sorted().joined(separator: "\n"),
