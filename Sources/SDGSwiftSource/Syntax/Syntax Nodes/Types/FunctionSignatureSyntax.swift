c971784dca67758443eb58517676bf5f160042a2
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-22 19:01:29 -0600

    Fixed resync.

--- Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/FunctionSignatureSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/FunctionSignatureSyntax.swift
@@ -24,7 +24,7 @@
     {
       return SyntaxFactory.makeFunctionSignature(
         input: input.normalizedForDeclaration(labelBehaviour: labelBehaviour),
-        asyncKeyword: asyncKeyword?.generallyNormalized(leadingTrivia: .spaces(1)),
+        asyncOrReasyncKeyword: asyncOrReasyncKeyword?.generallyNormalized(leadingTrivia: .spaces(1)),
         throwsOrRethrowsKeyword: throwsOrRethrowsKeyword?.generallyNormalized(
           leadingTrivia: .spaces(1)
         ),
@@ -37,7 +37,7 @@
     {
       return SyntaxFactory.makeFunctionSignature(
         input: input.forOverloadPattern(labelBehaviour: labelBehaviour),
-        asyncKeyword: nil,
+        asyncOrReasyncKeyword: nil,
         throwsOrRethrowsKeyword: nil,
         output: nil
       )
@@ -48,7 +48,7 @@
     {
       return SyntaxFactory.makeFunctionSignature(
         input: input.forName(labelBehaviour: labelBehaviour),
-        asyncKeyword: nil,
+        asyncOrReasyncKeyword: nil,
         throwsOrRethrowsKeyword: nil,
         output: nil
       )
