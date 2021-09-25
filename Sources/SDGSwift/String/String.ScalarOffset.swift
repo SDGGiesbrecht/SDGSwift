cdf02abc3cdf0312fdf541aeaded2acaa22ee5c7
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-09-24 18:57:41 -0600

    Removed debugging details.

--- Sources/SDGSwift/String/String.ScalarOffset.swift
+++ Sources/SDGSwift/String/String.ScalarOffset.swift
@@ -49,9 +49,7 @@ extension String {
 
     // #workaround(Swift 5.5, These are redundant overloads, but dodge segmentation faults caused by the compiler.)
     public static func −= (precedingValue: inout String.ScalarOffset, followingValue: Int) {
-      // #warning(Debugging...)
-      let minus = -followingValue  // @exempt(from: unicode)
-      precedingValue += minus
+      precedingValue += -followingValue  // @exempt(from: unicode)
     }
     public static func − (precedingValue: String.ScalarOffset, followingValue: Int) -> String.ScalarOffset {
       return nonmutatingVariant(of: −=, on: precedingValue, with: followingValue)
