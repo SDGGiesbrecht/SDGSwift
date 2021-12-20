020aa291d41ddd0a4c168f49e8d1a8ca4a709bf0
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2021-12-20 15:10:10 -0600

    Enabled Swift 5.2.2.

--- Sources/SDGSwiftPackageManager/SwiftCompiler.swift
+++ Sources/SDGSwiftPackageManager/SwiftCompiler.swift
@@ -26,7 +26,7 @@ extension SwiftCompiler {
 
   // MARK: - Properties
 
-  private static let compatibleVersions = SDGVersioning.Version(5, 5, 0)...Version(5, 5, 1)
+  private static let compatibleVersions = SDGVersioning.Version(5, 5, 0)...Version(5, 5, 2)
 
   internal static func swiftCLocation()
     -> Swift.Result<Foundation.URL, VersionedExternalProcessLocationError<SwiftCompiler>>
