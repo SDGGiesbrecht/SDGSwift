6942e4e0aa7329edd2a6e273ed2a6b3e16c7f757
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-05-05 20:17:13 -0600

    Enabled test.

--- Tests/SDGSwiftConfigurationTests/APITests.swift
+++ Tests/SDGSwiftConfigurationTests/APITests.swift
@@ -47,53 +47,106 @@ class APITests: SDGSwiftTestUtilities.TestCase {
       mock.option = "Mock"
       Configuration.queue(mock: mock)
     #else
-      #if !PLATFORM_SUFFERS_SEGMENTATION_FAULTS
-        try LocalizationSetting(orderOfPrecedence: ["en\u{2D}CA"]).do {
-          FileManager.default.delete(.cache)
-          defer { FileManager.default.delete(.cache) }
+      try LocalizationSetting(orderOfPrecedence: ["en\u{2D}CA"]).do {
+        FileManager.default.delete(.cache)
+        defer { FileManager.default.delete(.cache) }
 
-          XCTAssertEqual(SampleConfiguration().option, "Default")
-          testCodableConformance(of: SampleConfiguration(), uniqueTestName: "Sample Configuration")
+        XCTAssertEqual(SampleConfiguration().option, "Default")
+        testCodableConformance(of: SampleConfiguration(), uniqueTestName: "Sample Configuration")
 
-          let specifications = testSpecificationDirectory().appendingPathComponent("Configuration")
+        let specifications = testSpecificationDirectory().appendingPathComponent("Configuration")
 
-          let wherever = specifications.appendingPathComponent("Configured")
-          #if PLATFORM_NOT_SUPPORTED_BY_SWIFT_PM
-            // Silence warnings.
-            _ = wherever
-            func nothing() throws {}
-            try nothing()
-          #else
-            #if !PLATFORM_LACKS_GIT
-              // @example(configurationLoading)
-              // These refer to a real, working sample product.
-              // See its source for more details:
-              // https://github.com/SDGGiesbrecht/SDGSwift/tree/9.0.0/Sources/SampleConfiguration
-              let product = "SampleConfiguration"
-              let packageName = "SDGSwift"
-              let packageURL = URL(string: "https://github.com/SDGGiesbrecht/SDGSwift")!
-              let minimumMacOSVersion = Version(10, 13)
-              let version = Version(9, 0, 0)
-              let type = SampleConfiguration.self  // Import it first if necessary.
+        let wherever = specifications.appendingPathComponent("Configured")
+        #if PLATFORM_NOT_SUPPORTED_BY_SWIFT_PM
+          // Silence warnings.
+          _ = wherever
+          func nothing() throws {}
+          try nothing()
+        #else
+          #if !PLATFORM_LACKS_GIT
+            // @example(configurationLoading)
+            // These refer to a real, working sample product.
+            // See its source for more details:
+            // https://github.com/SDGGiesbrecht/SDGSwift/tree/9.0.0/Sources/SampleConfiguration
+            let product = "SampleConfiguration"
+            let packageName = "SDGSwift"
+            let packageURL = URL(string: "https://github.com/SDGGiesbrecht/SDGSwift")!
+            let minimumMacOSVersion = Version(10, 13)
+            let version = Version(9, 0, 0)
+            let type = SampleConfiguration.self  // Import it first if necessary.
 
-              // Assuming the above file is called “SampleConfigurationFile.swift”...
-              let name = UserFacing<StrictString, APILocalization>(
-                { _ in "SampleConfigurationFile" }
-              )
+            // Assuming the above file is called “SampleConfigurationFile.swift”...
+            let name = UserFacing<StrictString, APILocalization>(
+              { _ in "SampleConfigurationFile" }
+            )
 
-              // Change this to actually point at a directory containing the above file.
-              let configuredDirectory: URL = wherever
+            // Change this to actually point at a directory containing the above file.
+            let configuredDirectory: URL = wherever
 
-              // Context information can be provided. (Optional.)
-              let context = SampleContext(information: "Information")
+            // Context information can be provided. (Optional.)
+            let context = SampleContext(information: "Information")
 
-              // A log to collect progress reports while loading. (Optional.)
-              var log = String()
+            // A log to collect progress reports while loading. (Optional.)
+            var log = String()
 
-              let loadedConfiguration = try SampleConfiguration.load(
+            let loadedConfiguration = try SampleConfiguration.load(
+              configuration: type,
+              named: name,
+              from: configuredDirectory,
+              linkingAgainst: product,
+              in: packageName,
+              from: packageURL,
+              at: version,
+              minimumMacOSVersion: minimumMacOSVersion,
+              context: context,
+              reportProgress: { print($0, to: &log) }
+            ).get()
+            XCTAssertEqual(loadedConfiguration.option, "Configured")
+            // @endExample
+
+            print("", to: &log)
+            print("Cached", to: &log)
+            let cached = try SampleConfiguration.load(
+              configuration: type,
+              named: name,
+              from: configuredDirectory,
+              linkingAgainst: product,
+              in: packageName,
+              from: packageURL,
+              at: version,
+              minimumMacOSVersion: minimumMacOSVersion,
+              context: context,
+              reportProgress: { print($0, to: &log) }
+            ).get()
+            XCTAssertEqual(cached.option, "Configured")
+
+            print("", to: &log)
+            print("None", to: &log)
+            let none = specifications.appendingPathComponent("None")
+            XCTAssertEqual(
+              try SampleConfiguration.load(
                 configuration: type,
                 named: name,
-                from: configuredDirectory,
+                from: none,
+                linkingAgainst: product,
+                in: packageName,
+                from: packageURL,
+                at: version,
+                minimumMacOSVersion: minimumMacOSVersion,
+                context: context,
+                reportProgress: { print($0, to: &log) }
+              ).get().option,
+              "Default"
+            )
+
+            print("", to: &log)
+            print("Empty", to: &log)
+            let emptyDirectory = specifications.appendingPathComponent("Empty")
+            XCTAssertNil(
+              try? SampleConfiguration.load(
+                configuration: type,
+                named: name,
+                from: emptyDirectory,
                 linkingAgainst: product,
                 in: packageName,
                 from: packageURL,
@@ -102,185 +155,130 @@ class APITests: SDGSwiftTestUtilities.TestCase {
                 context: context,
                 reportProgress: { print($0, to: &log) }
               ).get()
-              XCTAssertEqual(loadedConfiguration.option, "Configured")
-              // @endExample
+            )
 
-              print("", to: &log)
-              print("Cached", to: &log)
-              let cached = try SampleConfiguration.load(
-                configuration: type,
-                named: name,
-                from: configuredDirectory,
-                linkingAgainst: product,
-                in: packageName,
-                from: packageURL,
-                at: version,
-                minimumMacOSVersion: minimumMacOSVersion,
-                context: context,
-                reportProgress: { print($0, to: &log) }
-              ).get()
-              XCTAssertEqual(cached.option, "Configured")
+            print("", to: &log)
+            print("Mock", to: &log)
 
-              print("", to: &log)
-              print("None", to: &log)
-              let none = specifications.appendingPathComponent("None")
-              XCTAssertEqual(
-                try SampleConfiguration.load(
-                  configuration: type,
-                  named: name,
-                  from: none,
-                  linkingAgainst: product,
-                  in: packageName,
-                  from: packageURL,
-                  at: version,
-                  minimumMacOSVersion: minimumMacOSVersion,
-                  context: context,
-                  reportProgress: { print($0, to: &log) }
-                ).get().option,
-                "Default"
-              )
+            let mock = SampleConfiguration()
+            mock.option = "Mock"
+            Configuration.queue(mock: mock)
+            let loadedMock = try SampleConfiguration.load(
+              configuration: type,
+              named: name,
+              from: configuredDirectory,
+              linkingAgainst: product,
+              in: packageName,
+              from: packageURL,
+              at: version,
+              minimumMacOSVersion: minimumMacOSVersion,
+              reportProgress: { print($0, to: &log) }
+            ).get()
+            XCTAssertEqual(loadedMock.option, "Mock")
 
-              print("", to: &log)
-              print("Empty", to: &log)
-              let emptyDirectory = specifications.appendingPathComponent("Empty")
-              XCTAssertNil(
-                try? SampleConfiguration.load(
-                  configuration: type,
-                  named: name,
-                  from: emptyDirectory,
-                  linkingAgainst: product,
-                  in: packageName,
-                  from: packageURL,
-                  at: version,
-                  minimumMacOSVersion: minimumMacOSVersion,
-                  context: context,
-                  reportProgress: { print($0, to: &log) }
-                ).get()
-              )
-
-              print("", to: &log)
-              print("Mock", to: &log)
-
-              let mock = SampleConfiguration()
-              mock.option = "Mock"
-              Configuration.queue(mock: mock)
-              let loadedMock = try SampleConfiguration.load(
-                configuration: type,
-                named: name,
-                from: configuredDirectory,
-                linkingAgainst: product,
-                in: packageName,
-                from: packageURL,
-                at: version,
-                minimumMacOSVersion: minimumMacOSVersion,
-                reportProgress: { print($0, to: &log) }
-              ).get()
-              XCTAssertEqual(loadedMock.option, "Mock")
-
-              func abbreviate(logEntry: String) {
-                let pattern =
-                  (logEntry + " ").scalars
-                  + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
-                  + "\n".scalars
-                log.scalars.replaceMatches(
-                  for: pattern,
-                  with: (logEntry + " [...]\n").scalars
-                )
-              }
-              func remove(logEntry: String) {
-                let pattern =
-                  logEntry.scalars
-                  + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
-                  + "\n".scalars
-                log.scalars.replaceMatches(for: pattern, with: "".scalars)
-              }
-              abbreviate(logEntry: "Fetching")
-              abbreviate(logEntry: "Completed resolution in")
-              abbreviate(logEntry: "Cloning")
-              abbreviate(logEntry: "Resolving")
-              log.lines.removeAll(where: { line in
-                return line.line.contains("Starting resolution using".scalars)
-              })
-
-              // These may occur out of order.
-              remove(logEntry: "Compile Swift Module")
-              let patternStart =
-                "[".scalars
-                + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
+            func abbreviate(logEntry: String) {
               let pattern =
-                patternStart
-                + "] Compiling ".scalars
+                (logEntry + " ").scalars
+                + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
+                + "\n".scalars
+              log.scalars.replaceMatches(
+                for: pattern,
+                with: (logEntry + " [...]\n").scalars
+              )
+            }
+            func remove(logEntry: String) {
+              let pattern =
+                logEntry.scalars
                 + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
                 + "\n".scalars
               log.scalars.replaceMatches(for: pattern, with: "".scalars)
-              remove(logEntry: "Linking")
-              remove(logEntry: "warning: invalid duplicate target dependency declaration")
-              remove(logEntry: "\u{27}llbuild\u{27}")
-              remove(logEntry: "Fetched")
-              remove(logEntry: "Computing version for")
-              remove(logEntry: "Computed")
-              remove(logEntry: "Creating working copy for")
-              remove(logEntry: "Working copy of")
-              remove(logEntry: "Computing")
+            }
+            abbreviate(logEntry: "Fetching")
+            abbreviate(logEntry: "Completed resolution in")
+            abbreviate(logEntry: "Cloning")
+            abbreviate(logEntry: "Resolving")
+            log.lines.removeAll(where: { line in
+              return line.line.contains("Starting resolution using".scalars)
+            })
 
-              let fractionPatternStart =
-                "[".scalars
-                + RepetitionPattern(ConditionalPattern({ $0.properties.isASCIIHexDigit }))
-                + "/".scalars
-              let fractionPattern =
-                fractionPatternStart
-                + RepetitionPattern(ConditionalPattern({ $0.properties.isASCIIHexDigit }))
-                + "]".scalars
-              log.scalars.replaceMatches(for: fractionPattern, with: "[[...]/[...]]".scalars)
-              let astPattern =
-                "[[...]/[...]] Wrapping AST for ".scalars
-                + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
-                + " for debugging\n".scalars
-              log.scalars.replaceMatches(for: astPattern, with: "".scalars)
+            // These may occur out of order.
+            remove(logEntry: "Compile Swift Module")
+            let patternStart =
+              "[".scalars
+              + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
+            let pattern =
+              patternStart
+              + "] Compiling ".scalars
+              + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
+              + "\n".scalars
+            log.scalars.replaceMatches(for: pattern, with: "".scalars)
+            remove(logEntry: "Linking")
+            remove(logEntry: "warning: invalid duplicate target dependency declaration")
+            remove(logEntry: "\u{27}llbuild\u{27}")
+            remove(logEntry: "Fetched")
+            remove(logEntry: "Computing version for")
+            remove(logEntry: "Computed")
+            remove(logEntry: "Creating working copy for")
+            remove(logEntry: "Working copy of")
+            remove(logEntry: "Computing")
 
-              var lines = log.lines.map { String(String.UnicodeScalarView($0.line)) }
-              lines.removeAll(where: { $0.contains("SQLITE_OPEN_FILEPROTECTION_") })
-              lines.removeAll(where: { $0.contains("[logging] misuse at line") })
-              log = lines.joined(separator: "\n")
-              let digits = ConditionalPattern({ $0 ∈ CharacterSet.decimalDigits })
-              let durationPatternOne = "(".scalars + RepetitionPattern(digits) + ".".scalars
-              let durationPattern = durationPatternOne + RepetitionPattern(digits) + "s)".scalars
-              log.scalars.replaceMatches(
-                for: durationPattern,
-                with: "([duration]s)".scalars
-              )
+            let fractionPatternStart =
+              "[".scalars
+              + RepetitionPattern(ConditionalPattern({ $0.properties.isASCIIHexDigit }))
+              + "/".scalars
+            let fractionPattern =
+              fractionPatternStart
+              + RepetitionPattern(ConditionalPattern({ $0.properties.isASCIIHexDigit }))
+              + "]".scalars
+            log.scalars.replaceMatches(for: fractionPattern, with: "[[...]/[...]]".scalars)
+            let astPattern =
+              "[[...]/[...]] Wrapping AST for ".scalars
+              + RepetitionPattern(ConditionalPattern({ $0 ≠ "\n" }))
+              + " for debugging\n".scalars
+            log.scalars.replaceMatches(for: astPattern, with: "".scalars)
 
-              if swift5_6 {
-                // #workaround(Swift 5.6, Log differs by platform due to SwiftSyntax.)
-                #if !os(Linux)
-                  compare(
-                    log,
-                    against: testSpecificationDirectory().appendingPathComponent(
-                      "Configuration Loading.txt"
-                    ),
-                    overwriteSpecificationInsteadOfFailing: false
-                  )
-                #endif
-              }
-            #endif
+            var lines = log.lines.map { String(String.UnicodeScalarView($0.line)) }
+            lines.removeAll(where: { $0.contains("SQLITE_OPEN_FILEPROTECTION_") })
+            lines.removeAll(where: { $0.contains("[logging] misuse at line") })
+            log = lines.joined(separator: "\n")
+            let digits = ConditionalPattern({ $0 ∈ CharacterSet.decimalDigits })
+            let durationPatternOne = "(".scalars + RepetitionPattern(digits) + ".".scalars
+            let durationPattern = durationPatternOne + RepetitionPattern(digits) + "s)".scalars
+            log.scalars.replaceMatches(
+              for: durationPattern,
+              with: "([duration]s)".scalars
+            )
+
+            if swift5_6 {
+              // #workaround(Swift 5.6, Log differs by platform due to SwiftSyntax.)
+              #if !os(Linux)
+                compare(
+                  log,
+                  against: testSpecificationDirectory().appendingPathComponent(
+                    "Configuration Loading.txt"
+                  ),
+                  overwriteSpecificationInsteadOfFailing: false
+                )
+              #endif
+            }
           #endif
+        #endif
 
-          FileManager.default.withTemporaryDirectory(appropriateFor: nil) { directory in
-            let url = directory.appendingPathComponent("no such URL")
-            _ = try? SampleConfiguration.load(
-              configuration: SampleConfiguration.self,
-              named: UserFacing<StrictString, APILocalization>({ _ in "..." }),
-              from: url,
-              linkingAgainst: "...",
-              in: "...",
-              from: url,
-              at: Version(1),
-              minimumMacOSVersion: Version(1),
-              context: SampleContext(information: "...")
-            ).get()
-          }
+        FileManager.default.withTemporaryDirectory(appropriateFor: nil) { directory in
+          let url = directory.appendingPathComponent("no such URL")
+          _ = try? SampleConfiguration.load(
+            configuration: SampleConfiguration.self,
+            named: UserFacing<StrictString, APILocalization>({ _ in "..." }),
+            from: url,
+            linkingAgainst: "...",
+            in: "...",
+            from: url,
+            at: Version(1),
+            minimumMacOSVersion: Version(1),
+            context: SampleContext(information: "...")
+          ).get()
         }
-      #endif
+      }
     #endif
   }
 
