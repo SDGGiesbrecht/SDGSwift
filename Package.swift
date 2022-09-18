434354428e0421c018f60c4332edffd9324b11bb
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-09-17 22:13:04 -0600

    Updated SwiftPM.

--- Package.swift
+++ Package.swift
@@ -113,9 +113,7 @@ let package = Package(
     ),
     .package(
       url: "https://github.com/SDGGiesbrecht/swift\u{2D}package\u{2D}manager",
-      // #warning(Branch.)
-      //exact: Version(0, 50700, 0)
-      branch: "swift‐5.7"
+      exact: Version(0, 50700, 1)
     ),
     .package(
       url: "https://github.com/apple/swift\u{2D}tools\u{2D}support\u{2D}core",
