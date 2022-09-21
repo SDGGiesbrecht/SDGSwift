a151047696b80d46f37477412e8c287064b552c4
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-09-20 21:49:37 -0600

    Split versions based on compiler.

--- Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/ProtocolDeclSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/ProtocolDeclSyntax.swift
@@ -31,31 +31,57 @@
     // MARK: - APIDeclaration
 
     internal func normalizedAPIDeclaration() -> ProtocolDeclSyntax {
-      return SyntaxFactory.makeProtocolDecl(
-        attributes: attributes?.normalizedForAPIDeclaration(),
-        modifiers: modifiers?.normalizedForAPIDeclaration(operatorFunction: false),
-        protocolKeyword: protocolKeyword.generallyNormalizedAndMissingInsteadOfNil(
-          trailingTrivia: .spaces(1)
-        ),
-        identifier: identifier.generallyNormalizedAndMissingInsteadOfNil(),
-        primaryAssociatedTypeClause: nil,  // #workaround(Not supported yet.)
-        inheritanceClause: nil,
-        genericWhereClause: genericWhereClause?.normalized(),
-        members: SyntaxFactory.makeBlankMemberDeclBlock()
-      )
+      #if !EXPERIMENTAL_TOOLCHAIN_VERSION
+        return SyntaxFactory.makeProtocolDecl(
+          attributes: attributes?.normalizedForAPIDeclaration(),
+          modifiers: modifiers?.normalizedForAPIDeclaration(operatorFunction: false),
+          protocolKeyword: protocolKeyword.generallyNormalizedAndMissingInsteadOfNil(
+            trailingTrivia: .spaces(1)
+          ),
+          identifier: identifier.generallyNormalizedAndMissingInsteadOfNil(),
+          inheritanceClause: nil,
+          genericWhereClause: genericWhereClause?.normalized(),
+          members: SyntaxFactory.makeBlankMemberDeclBlock()
+        )
+      #else
+        return SyntaxFactory.makeProtocolDecl(
+          attributes: attributes?.normalizedForAPIDeclaration(),
+          modifiers: modifiers?.normalizedForAPIDeclaration(operatorFunction: false),
+          protocolKeyword: protocolKeyword.generallyNormalizedAndMissingInsteadOfNil(
+            trailingTrivia: .spaces(1)
+          ),
+          identifier: identifier.generallyNormalizedAndMissingInsteadOfNil(),
+          primaryAssociatedTypeClause: nil,  // #workaround(Not supported yet.)
+          inheritanceClause: nil,
+          genericWhereClause: genericWhereClause?.normalized(),
+          members: SyntaxFactory.makeBlankMemberDeclBlock()
+        )
+      #endif
     }
 
     internal func name() -> ProtocolDeclSyntax {
-      return SyntaxFactory.makeProtocolDecl(
-        attributes: nil,
-        modifiers: nil,
-        protocolKeyword: SyntaxFactory.makeToken(.protocolKeyword, presence: .missing),
-        identifier: identifier,
-        primaryAssociatedTypeClause: nil,  // #workaround(Not supported yet.)
-        inheritanceClause: nil,
-        genericWhereClause: nil,
-        members: SyntaxFactory.makeBlankMemberDeclBlock()
-      )
+      #if !EXPERIMENTAL_TOOLCHAIN_VERSION
+        return SyntaxFactory.makeProtocolDecl(
+          attributes: nil,
+          modifiers: nil,
+          protocolKeyword: SyntaxFactory.makeToken(.protocolKeyword, presence: .missing),
+          identifier: identifier,
+          inheritanceClause: nil,
+          genericWhereClause: nil,
+          members: SyntaxFactory.makeBlankMemberDeclBlock()
+        )
+      #else
+        return SyntaxFactory.makeProtocolDecl(
+          attributes: nil,
+          modifiers: nil,
+          protocolKeyword: SyntaxFactory.makeToken(.protocolKeyword, presence: .missing),
+          identifier: identifier,
+          primaryAssociatedTypeClause: nil,  // #workaround(Not supported yet.)
+          inheritanceClause: nil,
+          genericWhereClause: nil,
+          members: SyntaxFactory.makeBlankMemberDeclBlock()
+        )
+      #endif
     }
 
     internal func identifierList() -> Set<String> {
