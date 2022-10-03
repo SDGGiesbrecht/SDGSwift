17572126d8a73ab4c2dcf9a9cf9ffdcbf132af50
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-10-03 16:53:04 -0600

    Removed dead tools udjustment.

--- Tests/SDGSwiftTestUtilities/Repositories.swift
+++ Tests/SDGSwiftTestUtilities/Repositories.swift
@@ -134,14 +134,6 @@ public let documentationTestPackages = ["PackageToDocument", "PackageToDocument2
           named: StrictString(mock.lastPathComponent),
           type: .library
         ).get()
-        // Revert tools version so that the initialized package can be loaded by the standard toolchain.
-        let manifest = repository.location.appendingPathComponent("Package.swift")
-        var string = try String(from: manifest)
-        string.replaceMatches(
-          for: "swift\u{2D}tools\u{2D}version: 5.7",
-          with: "swift\u{2D}tools\u{2D}version: 5.6"
-        )
-        try string.save(to: manifest)
         try test(repository)
       }
     }
