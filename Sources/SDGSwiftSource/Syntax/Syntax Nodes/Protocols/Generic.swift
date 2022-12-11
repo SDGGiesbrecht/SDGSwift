12a36c883004aac1f306decba4441d96f42c8cdc
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-12-11 17:27:47 -0600

    ...

--- Sources/SDGSwiftSource/Syntax/Syntax Nodes/Protocols/Generic.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax Nodes/Protocols/Generic.swift
@@ -20,21 +20,4 @@
   internal protocol Generic: Constrained {
     var genericParameterClause: GenericParameterClauseSyntax? { get }
   }
-
-  extension Generic {
-
-    internal func normalizedGenerics() -> (GenericParameterClauseSyntax?, GenericWhereClauseSyntax?)
-    {
-
-      var newGenericParemeterClause: GenericParameterClauseSyntax?
-      var newGenericWhereClause: GenericWhereClauseSyntax?
-      if let originalGenericParameterClause = genericParameterClause {
-        (newGenericParemeterClause, newGenericWhereClause) =
-          originalGenericParameterClause.normalizedForAPIDeclaration()
-      }
-
-      newGenericWhereClause.merge(with: genericWhereClause?.normalized())
-      return (newGenericParemeterClause, newGenericWhereClause)
-    }
-  }
 #endif
