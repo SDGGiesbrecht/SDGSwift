26538ba970179df1d6b732dabfac63ef20421d33
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-12-11 18:13:44 -0600

    ...

--- Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/AttributeSyntax.swift
+++ Sources/SDGSwiftSource/Syntax/Syntax Nodes/Types/AttributeSyntax.swift
@@ -46,65 +46,6 @@
       }
     }
 
-    internal func normalizedForAPIDeclaration() -> AttributeSyntax? {
-      let attribute = attributeName.text
-      if attribute.hasPrefix("_") {
-        // @exempt(from: tests) Currently unreachable because SwiftSyntax does not properly detect these in 0.40200.0).
-        return nil
-      }
-
-      switch attribute {
-      case "available":
-        // Availability
-        return normalized()
-      case "escaping", "autoclosure", "discardableResult":
-        // Call site
-        return normalized()
-      case "propertyWrapper":
-        // Usable as a property wrapper
-        return normalized()
-      case "objc", "nonobjc", "objcMembers":
-        // Objective‐C interface
-        return normalized()
-      case "IBOutlet", "IBDesignable", "IBInspectable", "GKInspectable":
-        // Xcode interface
-        return normalized()
-
-      case "inlinable", "usableFromInline", "inline", "dynamicMemberLookup", "convention":
-        // Implementation details
-        return nil
-      case "NSCopying", "NSManaged":
-        // Objective‐C implementation details
-        return SyntaxFactory.makeAttribute(
-          atSignToken: SyntaxFactory.makeToken(.atSign),
-          attributeName: SyntaxFactory.makeToken(
-            .contextualKeyword("objc"),
-            trailingTrivia: .spaces(1)
-          ),
-          leftParen: nil,
-          argument: nil,
-          rightParen: nil,
-          tokenList: nil
-        )
-      case "testable":  // @exempt(from: tests)
-        // Not relevant to API symbols
-        return nil
-      case "NSApplicationMain", "UIApplicationMain":  // @exempt(from: tests)
-        // Not relevant to API
-        return nil
-      case "requires_stored_property_inits", "warn_unqualified_access":  // @exempt(from: tests)
-        // Source checks
-        return nil
-      case "frozen":  // @exempt(from: tests)
-        // Not relevant outside library evolution mode.
-        return nil
-
-      default:  // @exempt(from: tests)
-        attribute.warnUnidentified()
-        return nil
-      }
-    }
-
     private func normalized() -> AttributeSyntax {
       if let argument = self.argument {
         return SyntaxFactory.makeAttribute(
@@ -138,26 +79,6 @@
       case discardability
       case escapability
       case autoclosure
-    }
-    private func group() -> Group {
-      switch attributeName.text {
-      case "available":
-        return .availability
-      case "escaping":
-        return .escapability
-      case "autoclosure":
-        return .autoclosure
-      case "discardableResult":
-        return .discardability
-      case "objc", "nonobjc", "objcMembers":
-        return .objectiveC
-      case "IBOutlet", "IBDesignable", "IBInspectable", "GKInspectable":
-        // Objective‐C implementation details
-        return .interfaceBuilder
-      default:  // @exempt(from: tests)
-        attributeName.text.warnUnidentified()
-        return .unknown
-      }
     }
   }
 #endif
