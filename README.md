b898b510e8f06aae1fbc683310e6b2d4bfda6944
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2022-12-16 19:37:46 -0600

    Version 12.0.0

--- README.md
+++ README.md
@@ -73,7 +73,7 @@ let package = Package(
     .package(
       name: "SDGSwift",
       url: "https://github.com/SDGGiesbrecht/SDGSwift",
-      from: Version(11, 1, 0)
+      from: Version(12, 0, 0)
     ),
   ],
   targets: [
--- Workspace.swift
+++ Workspace.swift
@@ -17,7 +17,7 @@ import WorkspaceConfiguration
 let configuration = WorkspaceConfiguration()
 configuration._applySDGDefaults()
 
-configuration.documentation.currentVersion = Version(11, 1, 0)
+configuration.documentation.currentVersion = Version(12, 0, 0)
 
 configuration.documentation.projectWebsite = URL(
   string: "https://sdggiesbrecht.github.io/SDGSwift"
