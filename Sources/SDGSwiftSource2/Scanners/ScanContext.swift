ba0954366c0bfeb03cdeedebe7eb45561f5b15c1
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-07 17:34:08 -0600

    Added compiled property.

--- Sources/SDGSwiftSource2/Scanners/ScanContext.swift
+++ Sources/SDGSwiftSource2/Scanners/ScanContext.swift
@@ -15,8 +15,19 @@
 /// The context of a node to be visited by a syntax scanner.
 public struct ScanContext {
 
+  // MARK: - Stored Properties
+
   /// The node’s ancestors in the global syntax context.
   ///
   /// The global syntax context is the syntax tree accessible by traversing fragmentation into parent contexts. That is, the root node of code in a documentation comment is the file containing the documentation comment, and if the code is in a fragmented code block, the node representing the code block itself will not participate in the graph, but rather each fragment will be individually related to the documentation node.
   public let globalAncestors: [SyntaxNode]
+
+  // MARK: - Computed Properties
+
+  /// Returns whether the context is relevant to compilation.
+  ///
+  /// Nodes within trivia are not compiled. Most notably, this method can be used to differentiate Swift code that is part of the main file (which is compiled) and Swift code provided as an example inside a documentation comment (which is not compiled).
+  public func isCompiled() -> Bool {
+    return globalAncestors.contains(where: { $0 is TriviaNode })
+  }
 }
