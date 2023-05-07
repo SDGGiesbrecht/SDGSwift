e3a1b120a7cbf493829f9b122d1612c906bab6a9
Jeremy David Giesbrecht <jeremy.g@sasktel.net>
2023-05-07 17:52:27 -0600

    Fixed compilation conditions.

--- Sources/SDGSwiftSource2/SwiftSyntax/SyntaxProtocol.swift
+++ Sources/SDGSwiftSource2/SwiftSyntax/SyntaxProtocol.swift
@@ -14,67 +14,67 @@
 
 #if !PLATFORM_NOT_SUPPORTED_BY_SWIFT_SYNTAX
   import SwiftSyntax
-#endif
 
-extension SyntaxProtocol {
+  extension SyntaxProtocol {
 
-  // MARK: - Source
+    // MARK: - Source
 
-  // #documentation(SyntaxNode.text())
-  /// Returns the node’s source text.
-  public func text() -> String {
-    var result = ""
-    write(to: &result)
-    return result
-  }
-
-  // MARK: - Ancestry
-
-  /// All the node’s ancestors in order from its immediate parent to the root node.
-  public var ancestors: AnySequence<Syntax> {
-    if let parent = self.parent {
-      return AnySequence(sequence(first: parent, next: { $0.parent }))
-    } else {
-      return AnySequence([])
+    // #documentation(SyntaxNode.text())
+    /// Returns the node’s source text.
+    public func text() -> String {
+      var result = ""
+      write(to: &result)
+      return result
     }
-  }
 
-  internal func isInIfConfigurationCondition() -> Bool {
-    var previousAncestor: Syntax = Syntax(self)
-    for ancestor in ancestors {
-      defer { previousAncestor = ancestor }
-      if let ifConfigurationClause = ancestor.as(IfConfigClauseSyntax.self),
-        let condition = Syntax(ifConfigurationClause.condition),
-        condition == previousAncestor
-      {
-        return true
+    // MARK: - Ancestry
+
+    /// All the node’s ancestors in order from its immediate parent to the root node.
+    public var ancestors: AnySequence<Syntax> {
+      if let parent = self.parent {
+        return AnySequence(sequence(first: parent, next: { $0.parent }))
+      } else {
+        return AnySequence([])
       }
     }
-    return false
-  }
 
-  // MARK: - Tokens
-
-  // @documentation(SDGSwiftSource.Syntax.firstToken())
-  /// Return the first token of the node.
-  public func firstToken() -> TokenSyntax? {
-    if let token = Syntax(self).as(TokenSyntax.self),
-      token.presence == .present
-    {
-      return token
+    internal func isInIfConfigurationCondition() -> Bool {
+      var previousAncestor: Syntax = Syntax(self)
+      for ancestor in ancestors {
+        defer { previousAncestor = ancestor }
+        if let ifConfigurationClause = ancestor.as(IfConfigClauseSyntax.self),
+          let condition = Syntax(ifConfigurationClause.condition),
+          condition == previousAncestor
+        {
+          return true
+        }
+      }
+      return false
     }
-    return children(viewMode: .sourceAccurate).lazy.compactMap({ $0.firstToken() }).first
-  }
 
-  // @documentation(SDGSwiftSource.Syntax.lastToken())
-  /// Returns the last token of the node.
-  public func lastToken() -> TokenSyntax? {
-    if let token = Syntax(self).as(TokenSyntax.self),
-      token.presence == .present
-    {
-      return token
+    // MARK: - Tokens
+
+    // @documentation(SDGSwiftSource.Syntax.firstToken())
+    /// Return the first token of the node.
+    public func firstToken() -> TokenSyntax? {
+      if let token = Syntax(self).as(TokenSyntax.self),
+        token.presence == .present
+      {
+        return token
+      }
+      return children(viewMode: .sourceAccurate).lazy.compactMap({ $0.firstToken() }).first
     }
-    return children(viewMode: .sourceAccurate).reversed()
-      .lazy.compactMap({ $0.lastToken() }).first
+
+    // @documentation(SDGSwiftSource.Syntax.lastToken())
+    /// Returns the last token of the node.
+    public func lastToken() -> TokenSyntax? {
+      if let token = Syntax(self).as(TokenSyntax.self),
+        token.presence == .present
+      {
+        return token
+      }
+      return children(viewMode: .sourceAccurate).reversed()
+        .lazy.compactMap({ $0.lastToken() }).first
+    }
   }
-}
+#endif
